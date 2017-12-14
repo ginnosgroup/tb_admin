1bc826a141a160e2b515ab140c59d441fc5af327
Larry <larry7696@gmail.com>
2017-12-14 12:40:26 +0800

    update official

---
+++ src/main/java/org/zhinanzhen/b/dao/OfficialDAO.java
@@ -0,0 +1,21 @@
+package org.zhinanzhen.b.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import org.zhinanzhen.b.dao.pojo.OfficialDO;
+
+public interface OfficialDAO {
+
+	public int addOfficial(OfficialDO officialDo);
+
+	public int updateOfficial(OfficialDO adviserDo);
+
+	public int countOfficial(@Param("name") String name, @Param("regionId") Integer regionId);
+
+	public List<OfficialDO> listOfficial(@Param("name") String name, @Param("regionId") Integer regionId,
+			@Param("offset") int offset, @Param("rows") int rows);
+
+	public OfficialDO getOfficialById(int id);
+
+}
---
+++ src/main/java/org/zhinanzhen/b/dao/pojo/OfficialDO.java
@@ -0,0 +1,26 @@
+package org.zhinanzhen.b.dao.pojo;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class OfficialDO {
+
+	private int id;
+
+	private Date gmtCreate;
+
+	private String name;
+
+	private String phone;
+
+	private String email;
+
+	private String state;
+
+	private String imageUrl;
+
+	private int regionId;
+
+}
