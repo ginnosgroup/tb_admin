23bddaf084c10bfc73c05e37096d9280d5a25f50
Larry <larry7696@gmail.com>
2017-12-25 09:36:46 +0800

    receive type

---
+++ src/main/java/org/zhinanzhen/b/dao/ReceiveTypeDAO.java
@@ -0,0 +1,16 @@
+package org.zhinanzhen.b.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import org.zhinanzhen.b.dao.pojo.ReceiveTypeDO;
+
+public interface ReceiveTypeDAO {
+
+	public int addReceiveType(ReceiveTypeDO receiveTypeDo);
+
+	public int updateReceiveType(ReceiveTypeDO receiveTypeDo);
+
+	public List<ReceiveTypeDO> listReceiveType(@Param("state") String state);
+
+}
---
+++ src/main/java/org/zhinanzhen/b/dao/pojo/ReceiveTypeDO.java
@@ -0,0 +1,16 @@
+package org.zhinanzhen.b.dao.pojo;
+
+import lombok.Data;
+
+@Data
+public class ReceiveTypeDO {
+
+	private int id;
+
+	private String name;
+
+	private String state;
+
+	private int weight;
+
+}
