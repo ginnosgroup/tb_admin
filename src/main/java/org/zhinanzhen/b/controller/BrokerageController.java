1d4738d4d9cb52824212143cb4ef14cca1a89394
Larry <larry7696@gmail.com>
2018-01-02 17:13:33 +0800

    brokerage controller

--- src/main/java/org/zhinanzhen/b/controller/BrokerageController.java
+++ src/main/java/org/zhinanzhen/b/controller/BrokerageController.java
@@ -1,5 +1,7 @@
 package org.zhinanzhen.b.controller;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
 
@@ -22,6 +24,36 @@ public class BrokerageController extends BaseController {
 
 	@Resource
 	BrokerageService brokerageService;
+
+	@RequestMapping(value = "/count", method = RequestMethod.GET)
+	@ResponseBody
+	public Response<Integer> countBrokerage(@RequestParam(value = "stardDate", required = false) String stardDate,
+			@RequestParam(value = "endDate", required = false) String endDate,
+			@RequestParam(value = "adviserId", required = false) Integer adviserId, HttpServletResponse response) {
+		try {
+			super.setGetHeader(response);
+			return new Response<Integer>(0, brokerageService.countBrokerage(stardDate, endDate, adviserId));
+		} catch (ServiceException e) {
+			return new Response<Integer>(1, e.getMessage(), null);
+		}
+	}
+
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	@ResponseBody
+	public Response<List<BrokerageDTO>> listBrokerage(
+			@RequestParam(value = "stardDate", required = false) String stardDate,
+			@RequestParam(value = "endDate", required = false) String endDate,
+			@RequestParam(value = "adviserId", required = false) Integer adviserId,
+			@RequestParam(value = "pageNum") int pageNum, @RequestParam(value = "pageSize") int pageSize,
+			HttpServletResponse response) {
+		try {
+			super.setGetHeader(response);
+			return new Response<List<BrokerageDTO>>(0,
+					brokerageService.listBrokerage(stardDate, endDate, adviserId, pageNum, pageSize));
+		} catch (ServiceException e) {
+			return new Response<List<BrokerageDTO>>(1, e.getMessage(), null);
+		}
+	}
 
 	@RequestMapping(value = "/get", method = RequestMethod.GET)
 	@ResponseBody
