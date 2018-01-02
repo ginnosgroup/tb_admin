94c4e4be7d18379378eebf5f13810229b3584f5c
Larry <larry7696@gmail.com>
2018-01-02 19:18:04 +0800

    school

--- src/main/java/org/zhinanzhen/b/dao/SchoolDAO.java
+++ src/main/java/org/zhinanzhen/b/dao/SchoolDAO.java
@@ -1,11 +1,13 @@
 package org.zhinanzhen.b.dao;
 
+import org.apache.ibatis.annotations.Param;
 import org.zhinanzhen.b.dao.pojo.SchoolDO;
 
 public interface SchoolDAO {
 
 	public int addSchool(SchoolDO schoolDO);
-	
-	public SchoolDO listSchool(String name);
+
+	public SchoolDO listSchool(@Param("name") String name, @Param("subject") String subject,
+			@Param("country") String country);
 
 }
