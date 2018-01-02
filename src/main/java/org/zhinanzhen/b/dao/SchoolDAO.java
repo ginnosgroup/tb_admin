359fe5c697ce553356a3f6c90ed6e7c3cf4ce6df
Larry <larry7696@gmail.com>
2018-01-02 19:26:09 +0800

    school

--- src/main/java/org/zhinanzhen/b/dao/SchoolDAO.java
+++ src/main/java/org/zhinanzhen/b/dao/SchoolDAO.java
@@ -7,7 +7,12 @@ public interface SchoolDAO {
 
 	public int addSchool(SchoolDO schoolDO);
 
+	public int updateSchool(@Param("name") String name, @Param("subject") String subject,
+			@Param("country") String country);
+
 	public SchoolDO listSchool(@Param("name") String name, @Param("subject") String subject,
 			@Param("country") String country);
+
+	public SchoolDO getSchoolById(int id);
 
 }
