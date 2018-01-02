f4bd9b0eac945dd9699056d6053a7e449289613c
Larry <larry7696@gmail.com>
2018-01-02 17:10:52 +0800

    brokerage controller

--- src/main/java/org/zhinanzhen/b/controller/BrokerageController.java
+++ src/main/java/org/zhinanzhen/b/controller/BrokerageController.java
@@ -1,19 +1,37 @@
 package org.zhinanzhen.b.controller;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.zhinanzhen.b.service.BrokerageService;
+import org.zhinanzhen.b.service.pojo.BrokerageDTO;
 import org.zhinanzhen.tb.controller.BaseController;
+import org.zhinanzhen.tb.controller.Response;
+import org.zhinanzhen.tb.service.ServiceException;
 
 @Controller
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/brokerage")
 public class BrokerageController extends BaseController {
-	
+
 	@Resource
 	BrokerageService brokerageService;
+
+	@RequestMapping(value = "/get", method = RequestMethod.GET)
+	@ResponseBody
+	public Response<BrokerageDTO> getBrokerage(@RequestParam(value = "id") int id, HttpServletResponse response) {
+		try {
+			super.setGetHeader(response);
+			return new Response<BrokerageDTO>(0, brokerageService.getBrokerageById(id));
+		} catch (ServiceException e) {
+			return new Response<BrokerageDTO>(1, e.getMessage(), null);
+		}
+	}
 
 }
