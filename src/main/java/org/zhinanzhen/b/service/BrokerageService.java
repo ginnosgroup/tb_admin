40bc26ce3b56392e4a723c7d988475e4e94f6e75
Larry <larry7696@gmail.com>
2018-01-02 13:12:01 +0800

    brokerage service

--- src/main/java/org/zhinanzhen/b/service/BrokerageService.java
+++ src/main/java/org/zhinanzhen/b/service/BrokerageService.java
@@ -3,18 +3,19 @@ package org.zhinanzhen.b.service;
 import java.util.List;
 
 import org.zhinanzhen.b.service.pojo.BrokerageDTO;
+import org.zhinanzhen.tb.service.ServiceException;
 
 public interface BrokerageService {
 
-	public int addBrokerage(BrokerageDTO brokerageDto);
+	public int addBrokerage(BrokerageDTO brokerageDto) throws ServiceException;
 
-	public int updateBrokerage(BrokerageDTO brokerageDto);
+	public int updateBrokerage(BrokerageDTO brokerageDto) throws ServiceException;
 
-	public int countBrokerage(String stardDate, String endDate, Integer adviserId);
+	public int countBrokerage(String stardDate, String endDate, Integer adviserId) throws ServiceException;
 
 	public List<BrokerageDTO> listBrokerage(String stardDate, String endDate, Integer adviserId, int pageNum,
-			int pageSize);
+			int pageSize) throws ServiceException;
 
-	public BrokerageDTO getBrokerageById(int id);
+	public BrokerageDTO getBrokerageById(int id) throws ServiceException;
 
 }
--- src/main/java/org/zhinanzhen/b/service/impl/BrokerageServiceImpl.java
+++ src/main/java/org/zhinanzhen/b/service/impl/BrokerageServiceImpl.java
@@ -1,44 +1,117 @@
 package org.zhinanzhen.b.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Service;
+import org.zhinanzhen.b.dao.BrokerageDAO;
+import org.zhinanzhen.b.dao.pojo.BrokerageDO;
 import org.zhinanzhen.b.service.BrokerageService;
 import org.zhinanzhen.b.service.pojo.BrokerageDTO;
+import org.zhinanzhen.tb.service.ServiceException;
 import org.zhinanzhen.tb.service.impl.BaseService;
+
+import com.ikasoa.core.thrift.ErrorCodeEnum;
 
 @Service("BrokerageService")
 public class BrokerageServiceImpl extends BaseService implements BrokerageService {
 
+	@Resource
+	private BrokerageDAO brokerageDao;
+
 	@Override
-	public int addBrokerage(BrokerageDTO brokerageDto) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int addBrokerage(BrokerageDTO brokerageDto) throws ServiceException {
+		if (brokerageDto == null) {
+			ServiceException se = new ServiceException("brokerageDto is null !");
+			se.setCode(ErrorCodeEnum.PARAMETER_ERROR.code());
+			throw se;
+		}
+		try {
+			BrokerageDO brokerageDo = mapper.map(brokerageDto, BrokerageDO.class);
+			if (brokerageDao.addBrokerage(brokerageDo) > 0) {
+				return brokerageDo.getId();
+			} else {
+				return 0;
+			}
+		} catch (Exception e) {
+			ServiceException se = new ServiceException(e);
+			se.setCode(ErrorCodeEnum.OTHER_ERROR.code());
+			throw se;
+		}
 	}
 
 	@Override
-	public int updateBrokerage(BrokerageDTO brokerageDto) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int updateBrokerage(BrokerageDTO brokerageDto) throws ServiceException {
+		if (brokerageDto == null) {
+			ServiceException se = new ServiceException("brokerageDto is null !");
+			se.setCode(ErrorCodeEnum.PARAMETER_ERROR.code());
+			throw se;
+		}
+		try {
+			BrokerageDO brokerageDo = mapper.map(brokerageDto, BrokerageDO.class);
+			return brokerageDao.updateBrokerage(brokerageDo);
+		} catch (Exception e) {
+			ServiceException se = new ServiceException(e);
+			se.setCode(ErrorCodeEnum.OTHER_ERROR.code());
+			throw se;
+		}
 	}
 
 	@Override
-	public int countBrokerage(String stardDate, String endDate, Integer adviserId) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int countBrokerage(String stardDate, String endDate, Integer adviserId) throws ServiceException {
+		return brokerageDao.countBrokerage(stardDate, endDate, adviserId);
 	}
 
 	@Override
 	public List<BrokerageDTO> listBrokerage(String stardDate, String endDate, Integer adviserId, int pageNum,
-			int pageSize) {
-		// TODO Auto-generated method stub
-		return null;
+			int pageSize) throws ServiceException {
+		if (pageNum < 0) {
+			pageNum = DEFAULT_PAGE_NUM;
+		}
+		if (pageSize < 0) {
+			pageSize = DEFAULT_PAGE_SIZE;
+		}
+		List<BrokerageDTO> brokerageDtoList = new ArrayList<BrokerageDTO>();
+		List<BrokerageDO> brokerageDoList = new ArrayList<BrokerageDO>();
+		try {
+			brokerageDoList = brokerageDao.listBrokerage(stardDate, endDate, adviserId, pageNum * pageSize, pageSize);
+			if (brokerageDoList == null) {
+				return null;
+			}
+		} catch (Exception e) {
+			ServiceException se = new ServiceException(e);
+			se.setCode(ErrorCodeEnum.EXECUTE_ERROR.code());
+			throw se;
+		}
+		for (BrokerageDO brokerageDo : brokerageDoList) {
+			BrokerageDTO brokerageDto = mapper.map(brokerageDo, BrokerageDTO.class);
+			brokerageDtoList.add(brokerageDto);
+		}
+		return brokerageDtoList;
 	}
 
 	@Override
-	public BrokerageDTO getBrokerageById(int id) {
-		// TODO Auto-generated method stub
-		return null;
+	public BrokerageDTO getBrokerageById(int id) throws ServiceException {
+		if (id <= 0) {
+			ServiceException se = new ServiceException("id error !");
+			se.setCode(ErrorCodeEnum.PARAMETER_ERROR.code());
+			throw se;
+		}
+		BrokerageDTO brokerageDto = null;
+		try {
+			BrokerageDO brokerageDo = brokerageDao.getBrokerageById(id);
+			if (brokerageDo == null) {
+				return null;
+			}
+			brokerageDto = mapper.map(brokerageDo, BrokerageDTO.class);
+		} catch (Exception e) {
+			ServiceException se = new ServiceException(e);
+			se.setCode(ErrorCodeEnum.OTHER_ERROR.code());
+			throw se;
+		}
+		return brokerageDto;
 	}
 
 }
