83f31568902fb21f98416bd51a304af1ef746720
Larry <larry7696@gmail.com>
2018-01-03 18:44:43 +0800

    school

--- src/main/java/org/zhinanzhen/b/service/SchoolService.java
+++ src/main/java/org/zhinanzhen/b/service/SchoolService.java
@@ -1,17 +1,19 @@
 package org.zhinanzhen.b.service;
 
-import org.zhinanzhen.b.dao.pojo.SchoolDO;
+import java.util.List;
+
+import org.zhinanzhen.b.service.pojo.SchoolDTO;
 import org.zhinanzhen.tb.service.ServiceException;
 
 public interface SchoolService {
 
-	public int addSchool(SchoolDO schoolDO) throws ServiceException;
+	public int addSchool(SchoolDTO schoolDto) throws ServiceException;
 
 	public int updateSchool(String name, String subject, String country) throws ServiceException;
 
-	public SchoolDO listSchool(String name, String subject, String country) throws ServiceException;
+	public List<SchoolDTO> listSchool(String name, String subject, String country) throws ServiceException;
 
-	public SchoolDO getSchoolById(int id) throws ServiceException;
+	public SchoolDTO getSchoolById(int id) throws ServiceException;
 
 	public int deleteSchoolById(int id) throws ServiceException;
 
--- src/main/java/org/zhinanzhen/b/service/impl/SchoolServiceImpl.java
+++ src/main/java/org/zhinanzhen/b/service/impl/SchoolServiceImpl.java
@@ -1,18 +1,44 @@
 package org.zhinanzhen.b.service.impl;
 
+import java.util.List;
+
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Service;
+import org.zhinanzhen.b.dao.SchoolDAO;
 import org.zhinanzhen.b.dao.pojo.SchoolDO;
 import org.zhinanzhen.b.service.SchoolService;
+import org.zhinanzhen.b.service.pojo.SchoolDTO;
 import org.zhinanzhen.tb.service.ServiceException;
 import org.zhinanzhen.tb.service.impl.BaseService;
 
+import com.ikasoa.core.thrift.ErrorCodeEnum;
+
 @Service("SchoolService")
 public class SchoolServiceImpl extends BaseService implements SchoolService {
+	
+	@Resource
+	private SchoolDAO schoolDao;
 
 	@Override
-	public int addSchool(SchoolDO schoolDO) throws ServiceException {
-		// TODO Auto-generated method stub
-		return 0;
+	public int addSchool(SchoolDTO schoolDto) throws ServiceException {
+		if (schoolDto == null) {
+			ServiceException se = new ServiceException("schoolDto is null !");
+			se.setCode(ErrorCodeEnum.PARAMETER_ERROR.code());
+			throw se;
+		}
+		try {
+			SchoolDO schoolDo = mapper.map(schoolDto, SchoolDO.class);
+			if (schoolDao.addSchool(schoolDo) > 0) {
+				return schoolDo.getId();
+			} else {
+				return 0;
+			}
+		} catch (Exception e) {
+			ServiceException se = new ServiceException(e);
+			se.setCode(ErrorCodeEnum.OTHER_ERROR.code());
+			throw se;
+		}
 	}
 
 	@Override
@@ -22,13 +48,13 @@ public class SchoolServiceImpl extends BaseService implements SchoolService {
 	}
 
 	@Override
-	public SchoolDO listSchool(String name, String subject, String country) throws ServiceException {
+	public List<SchoolDTO> listSchool(String name, String subject, String country) throws ServiceException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public SchoolDO getSchoolById(int id) throws ServiceException {
+	public SchoolDTO getSchoolById(int id) throws ServiceException {
 		// TODO Auto-generated method stub
 		return null;
 	}
