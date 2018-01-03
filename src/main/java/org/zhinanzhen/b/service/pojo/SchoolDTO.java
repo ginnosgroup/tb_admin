d764eac7bd3eb736c2bd58752bb4ffae85f435db
Larry <larry7696@gmail.com>
2018-01-03 18:44:24 +0800

    school

---
+++ src/main/java/org/zhinanzhen/b/service/pojo/SchoolDTO.java
@@ -0,0 +1,16 @@
+package org.zhinanzhen.b.service.pojo;
+
+import lombok.Data;
+
+@Data
+public class SchoolDTO {
+	
+	private int id;
+
+	private String name;
+	
+	private String subject;
+
+	private String country;
+
+}
