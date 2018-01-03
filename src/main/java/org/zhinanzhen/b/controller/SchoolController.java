9402183a4e146843304f599e7f4e61f209cf496d
Larry <larry7696@gmail.com>
2018-01-03 19:12:01 +0800

    school controller

--- src/main/java/org/zhinanzhen/b/controller/SchoolController.java
+++ src/main/java/org/zhinanzhen/b/controller/SchoolController.java
@@ -1,13 +1,19 @@
 package org.zhinanzhen.b.controller;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.zhinanzhen.b.service.SchoolService;
 import org.zhinanzhen.tb.controller.BaseController;
 
 @Controller
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/school")
 public class SchoolController extends BaseController {
+
+	@Resource
+	SchoolService schoolService;
 
 }
