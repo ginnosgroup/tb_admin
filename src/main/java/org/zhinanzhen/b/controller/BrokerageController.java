6cd84cf2c0859ba825a91c08a30f56348641b454
Larry <larry7696@gmail.com>
2018-01-05 15:52:16 +0800

    brokerage

--- src/main/java/org/zhinanzhen/b/controller/BrokerageController.java
+++ src/main/java/org/zhinanzhen/b/controller/BrokerageController.java
@@ -1,8 +1,10 @@
 package org.zhinanzhen.b.controller;
 
+import java.util.Date;
 import java.util.List;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
@@ -17,6 +19,8 @@ import org.zhinanzhen.tb.controller.BaseController;
 import org.zhinanzhen.tb.controller.Response;
 import org.zhinanzhen.tb.service.ServiceException;
 
+import com.ikasoa.core.utils.StringUtil;
+
 @Controller
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/brokerage")
@@ -24,6 +28,53 @@ public class BrokerageController extends BaseController {
 
 	@Resource
 	BrokerageService brokerageService;
+
+	@RequestMapping(value = "/update", method = RequestMethod.POST)
+	@ResponseBody
+	public Response<BrokerageDTO> updateBrokerage(@RequestParam(value = "id") int id,
+			@RequestParam(value = "userId", required = false) String userId,
+			@RequestParam(value = "receiveTypeId", required = false) String receiveTypeId,
+			@RequestParam(value = "receiveDate", required = false) String receiveDate,
+			@RequestParam(value = "serviceId", required = false) String serviceId,
+			@RequestParam(value = "receivable", required = false) String receivable,
+			@RequestParam(value = "received", required = false) String received,
+			@RequestParam(value = "amount", required = false) String amount, HttpServletRequest request,
+			HttpServletResponse response) {
+		try {
+			super.setPostHeader(response);
+			BrokerageDTO brokerageDto = new BrokerageDTO();
+			brokerageDto.setId(id);
+			if (StringUtil.isNotEmpty(userId)) {
+				brokerageDto.setUserId(Integer.parseInt(userId));
+			}
+			if (StringUtil.isNotEmpty(receiveTypeId)) {
+				brokerageDto.setReceiveTypeId(Integer.parseInt(receiveTypeId));
+			}
+			if (StringUtil.isNotEmpty(receiveDate)) {
+				brokerageDto.setReceiveDate(new Date(Long.parseLong(receiveDate)));
+			}
+			if (StringUtil.isNotEmpty(serviceId)) {
+				brokerageDto.setServiceId(Integer.parseInt(serviceId));
+			}
+			if (StringUtil.isNotEmpty(receivable)) {
+				brokerageDto.setReceivable(Double.parseDouble(receivable));
+			}
+			if (StringUtil.isNotEmpty(received)) {
+				brokerageDto.setReceived(Double.parseDouble(received));
+			}
+			double commission = brokerageDto.getAmount();
+			brokerageDto.setGst(commission / 11);
+			brokerageDto.setDeductGst(commission - brokerageDto.getGst());
+			brokerageDto.setBonus(brokerageDto.getDeductGst() * 0.1);
+			if (brokerageService.updateBrokerage(brokerageDto) > 0) {
+				return new Response<BrokerageDTO>(0, brokerageDto);
+			} else {
+				return new Response<BrokerageDTO>(1, "修改失败.", null);
+			}
+		} catch (ServiceException e) {
+			return new Response<BrokerageDTO>(e.getCode(), e.getMessage(), null);
+		}
+	}
 
 	@RequestMapping(value = "/count", method = RequestMethod.GET)
 	@ResponseBody
