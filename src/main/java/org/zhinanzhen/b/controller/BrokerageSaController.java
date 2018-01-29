905f5a93756be560b0c2d6ed3c7428160d74fdb9
Larry <larry7696@gmail.com>
2018-01-29 09:54:36 +0800

    brokerage sa controller

--- src/main/java/org/zhinanzhen/b/controller/BrokerageSaController.java
+++ src/main/java/org/zhinanzhen/b/controller/BrokerageSaController.java
@@ -1,5 +1,7 @@
 package org.zhinanzhen.b.controller;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
 
@@ -23,9 +25,59 @@ public class BrokerageSaController extends BaseController {
 	@Resource
 	BrokerageSaService brokerageSaService;
 
+	@RequestMapping(value = "/count", method = RequestMethod.GET)
+	@ResponseBody
+	public Response<Integer> countBrokerage(@RequestParam(value = "keyword", required = false) String keyword,
+			@RequestParam(value = "startCreateDate", required = false) String startCreateDate,
+			@RequestParam(value = "endCreateDate", required = false) String endCreateDate,
+			@RequestParam(value = "startHandlingDate", required = false) String startHandlingDate,
+			@RequestParam(value = "endHandlingDate", required = false) String endHandlingDate,
+			@RequestParam(value = "adviserId", required = false) Integer adviserId,
+			@RequestParam(value = "schoolId", required = false) Integer schoolId, HttpServletResponse response) {
+		try {
+			super.setGetHeader(response);
+			return new Response<Integer>(0, brokerageSaService.countBrokerageSa(keyword, startCreateDate, endCreateDate,
+					startHandlingDate, endHandlingDate, adviserId, schoolId));
+		} catch (ServiceException e) {
+			return new Response<Integer>(1, e.getMessage(), null);
+		}
+	}
+
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	@ResponseBody
+	public Response<List<BrokerageSaDTO>> listBrokerage(
+			@RequestParam(value = "keyword", required = false) String keyword,
+			@RequestParam(value = "startCreateDate", required = false) String startCreateDate,
+			@RequestParam(value = "endCreateDate", required = false) String endCreateDate,
+			@RequestParam(value = "startHandlingDate", required = false) String startHandlingDate,
+			@RequestParam(value = "endHandlingDate", required = false) String endHandlingDate,
+			@RequestParam(value = "adviserId", required = false) Integer adviserId,
+			@RequestParam(value = "schoolId", required = false) Integer schoolId,
+			@RequestParam(value = "pageNum") int pageNum, @RequestParam(value = "pageSize") int pageSize,
+			HttpServletResponse response) {
+		try {
+			super.setGetHeader(response);
+			return new Response<List<BrokerageSaDTO>>(0, brokerageSaService.listBrokerageSa(keyword, startCreateDate,
+					endCreateDate, startHandlingDate, endHandlingDate, adviserId, schoolId, pageNum, pageSize));
+		} catch (ServiceException e) {
+			return new Response<List<BrokerageSaDTO>>(1, e.getMessage(), null);
+		}
+	}
+
+	@RequestMapping(value = "/get", method = RequestMethod.GET)
+	@ResponseBody
+	public Response<BrokerageSaDTO> getBrokerageSa(@RequestParam(value = "id") int id, HttpServletResponse response) {
+		try {
+			super.setGetHeader(response);
+			return new Response<BrokerageSaDTO>(0, brokerageSaService.getBrokerageSaById(id));
+		} catch (ServiceException e) {
+			return new Response<BrokerageSaDTO>(1, e.getMessage(), null);
+		}
+	}
+
 	@RequestMapping(value = "/close", method = RequestMethod.GET)
 	@ResponseBody
-	public Response<Integer> closeBrokerage(@RequestParam(value = "id") int id, HttpServletResponse response) {
+	public Response<Integer> closeBrokerageSa(@RequestParam(value = "id") int id, HttpServletResponse response) {
 		try {
 			super.setGetHeader(response);
 			BrokerageSaDTO brokerageSaDto = brokerageSaService.getBrokerageSaById(id);
@@ -38,7 +90,7 @@ public class BrokerageSaController extends BaseController {
 
 	@RequestMapping(value = "/reopen", method = RequestMethod.GET)
 	@ResponseBody
-	public Response<Integer> reopenBrokerage(@RequestParam(value = "id") int id, HttpServletResponse response) {
+	public Response<Integer> reopenBrokerageSa(@RequestParam(value = "id") int id, HttpServletResponse response) {
 		try {
 			super.setGetHeader(response);
 			BrokerageSaDTO brokerageSaDto = brokerageSaService.getBrokerageSaById(id);
