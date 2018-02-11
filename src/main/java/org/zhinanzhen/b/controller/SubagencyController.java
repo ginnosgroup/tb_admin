979044610b6605dc9015cc96d7d014016e6bd6f6
Larry <larry7696@gmail.com>
2018-02-11 14:59:32 +0800

    subagency controller

--- src/main/java/org/zhinanzhen/b/controller/SubagencyController.java
+++ src/main/java/org/zhinanzhen/b/controller/SubagencyController.java
@@ -3,6 +3,7 @@ package org.zhinanzhen.b.controller;
 import java.util.List;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
@@ -17,6 +18,8 @@ import org.zhinanzhen.tb.controller.BaseController;
 import org.zhinanzhen.tb.controller.Response;
 import org.zhinanzhen.tb.service.ServiceException;
 
+import com.ikasoa.core.utils.StringUtil;
+
 @Controller
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/subagency")
@@ -24,6 +27,49 @@ public class SubagencyController extends BaseController {
 
 	@Resource
 	SubagencyService subagencyService;
+
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	@ResponseBody
+	public Response<Integer> addSubagency(@RequestParam(value = "name") String name,
+			@RequestParam(value = "commissionRate") String commissionRate, HttpServletRequest request,
+			HttpServletResponse response) {
+		try {
+			super.setPostHeader(response);
+			SubagencyDTO subagencyDto = new SubagencyDTO();
+			subagencyDto.setName(name);
+			subagencyDto.setCommissionRate(Double.parseDouble(commissionRate.trim()));
+			if (subagencyService.addSubagency(subagencyDto) > 0) {
+				return new Response<Integer>(0, subagencyDto.getId());
+			} else {
+				return new Response<Integer>(1, "创建失败.", 0);
+			}
+		} catch (ServiceException e) {
+			return new Response<Integer>(e.getCode(), e.getMessage(), 0);
+		}
+	}
+
+	@RequestMapping(value = "/update", method = RequestMethod.POST)
+	@ResponseBody
+	public Response<Integer> updateSubagency(@RequestParam(value = "id") int id,
+			@RequestParam(value = "name", required = false) String name,
+			@RequestParam(value = "commissionRate", required = false) String commissionRate, HttpServletRequest request,
+			HttpServletResponse response) {
+		try {
+			super.setPostHeader(response);
+			Double _commissionRate = null;
+			if (StringUtil.isNotEmpty(commissionRate)) {
+				_commissionRate = Double.parseDouble(commissionRate.trim());
+			}
+			int i = subagencyService.updateSubagency(name, _commissionRate);
+			if (i > 0) {
+				return new Response<Integer>(0, i);
+			} else {
+				return new Response<Integer>(1, "修改失败.", 0);
+			}
+		} catch (ServiceException e) {
+			return new Response<Integer>(e.getCode(), e.getMessage(), null);
+		}
+	}
 
 	@RequestMapping(value = "/list", method = RequestMethod.GET)
 	@ResponseBody
