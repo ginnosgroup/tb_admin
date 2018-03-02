0dd177913e81709af2e99bbdb664dce8556ca075
Larry <larry7696@gmail.com>
2018-03-02 17:16:33 +0800

    remind

---
+++ src/main/java/org/zhinanzhen/b/service/impl/RemindServiceImpl.java
@@ -0,0 +1,37 @@
+package org.zhinanzhen.b.service.impl;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+import org.zhinanzhen.b.dao.RemindDAO;
+import org.zhinanzhen.b.service.RemindService;
+import org.zhinanzhen.b.service.pojo.RemindDTO;
+import org.zhinanzhen.tb.service.impl.BaseService;
+
+@Service("OfficialService")
+public class RemindServiceImpl extends BaseService implements RemindService {
+
+	@Resource
+	private RemindDAO remindDao;
+
+	@Override
+	public int addRemind(RemindDTO remindDto) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<RemindDTO> listRemindBySchoolBrokerageSaId(int schoolBrokerageSaId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int deleteSchoolById(int id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
