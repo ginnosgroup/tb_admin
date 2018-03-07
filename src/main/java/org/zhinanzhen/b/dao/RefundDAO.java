36c905914a1357eb3ff45762b9cec0ab47c2d5ca
Larry <larry7696@gmail.com>
2018-03-07 15:57:51 +0800

    refund

--- src/main/java/org/zhinanzhen/b/dao/RefundDAO.java
+++ src/main/java/org/zhinanzhen/b/dao/RefundDAO.java
@@ -3,9 +3,11 @@ package org.zhinanzhen.b.dao;
 import org.zhinanzhen.b.dao.pojo.RefundDO;
 
 public interface RefundDAO {
-	
+
 	public int addRefund(RefundDO refundDO);
-	
+
+	public int updateRefund(RefundDO refundDO);
+
 	public int deleteRefund(int id);
 
 }
