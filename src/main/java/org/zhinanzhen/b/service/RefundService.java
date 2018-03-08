d878fc0bd02bc2ace81b4a34338805333767ce48
Larry <larry7696@gmail.com>
2018-03-08 19:17:28 +0800

    refund

---
+++ src/main/java/org/zhinanzhen/b/service/RefundService.java
@@ -0,0 +1,25 @@
+package org.zhinanzhen.b.service;
+
+import java.util.List;
+
+import org.zhinanzhen.b.service.pojo.RefundDTO;
+import org.zhinanzhen.tb.service.ServiceException;
+
+public interface RefundService {
+
+	public int addRefund(RefundDTO refundDto) throws ServiceException;
+
+	public int updateRefund(RefundDTO refundDto) throws ServiceException;
+
+	public int countRefund(String keyword, String startHandlingDate, String endHandlingDate, String startDate,
+			String endDate, Integer adviserId, Integer officialId) throws ServiceException;
+
+	public List<RefundDTO> listRefund(String keyword, String startHandlingDate, String endHandlingDate,
+			String startDate, String endDate, Integer adviserId, Integer officialId, int pageNum, int pageSize)
+			throws ServiceException;
+
+	public RefundDTO getRefundById(int id) throws ServiceException;
+
+	public int deleteRefundById(int id) throws ServiceException;
+
+}
