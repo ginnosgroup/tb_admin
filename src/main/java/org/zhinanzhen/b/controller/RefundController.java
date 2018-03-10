24252800dce444fb58a589b266ab8ad27e380ab1
Larry <larry7696@gmail.com>
2018-03-10 12:38:37 +0800

    refund controller

---
+++ src/main/java/org/zhinanzhen/b/controller/RefundController.java
@@ -0,0 +1,36 @@
+package org.zhinanzhen.b.controller;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.zhinanzhen.b.service.RefundService;
+import org.zhinanzhen.tb.controller.BaseController;
+import org.zhinanzhen.tb.controller.Response;
+import org.zhinanzhen.tb.service.ServiceException;
+
+@Controller
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/refund")
+public class RefundController extends BaseController {
+
+	@Resource
+	RefundService refundService;
+
+	@RequestMapping(value = "/delete", method = RequestMethod.GET)
+	@ResponseBody
+	public Response<Integer> deleteRefund(@RequestParam(value = "id") int id, HttpServletResponse response) {
+		try {
+			super.setGetHeader(response);
+			return new Response<Integer>(0, refundService.deleteRefundById(id));
+		} catch (ServiceException e) {
+			return new Response<Integer>(1, e.getMessage(), 0);
+		}
+	}
+
+}
