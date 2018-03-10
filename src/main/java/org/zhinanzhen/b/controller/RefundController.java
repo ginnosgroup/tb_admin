879ede3e38fb6ddf04c40756d1557e7bc0d547c0
Larry <larry7696@gmail.com>
2018-03-10 13:47:00 +0800

    refund controller

--- src/main/java/org/zhinanzhen/b/controller/RefundController.java
+++ src/main/java/org/zhinanzhen/b/controller/RefundController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.zhinanzhen.b.service.RefundService;
+import org.zhinanzhen.b.service.pojo.RefundDTO;
 import org.zhinanzhen.tb.controller.BaseController;
 import org.zhinanzhen.tb.controller.Response;
 import org.zhinanzhen.tb.service.ServiceException;
@@ -21,6 +22,43 @@ public class RefundController extends BaseController {
 
 	@Resource
 	RefundService refundService;
+
+	@RequestMapping(value = "/get", method = RequestMethod.GET)
+	@ResponseBody
+	public Response<RefundDTO> getRefund(@RequestParam(value = "id") int id, HttpServletResponse response) {
+		try {
+			super.setGetHeader(response);
+			return new Response<RefundDTO>(0, refundService.getRefundById(id));
+		} catch (ServiceException e) {
+			return new Response<RefundDTO>(1, e.getMessage(), null);
+		}
+	}
+
+	@RequestMapping(value = "/close", method = RequestMethod.GET)
+	@ResponseBody
+	public Response<Integer> closeRefund(@RequestParam(value = "id") int id, HttpServletResponse response) {
+		try {
+			super.setGetHeader(response);
+			RefundDTO refundDto = refundService.getRefundById(id);
+			refundDto.setClose(true);
+			return new Response<Integer>(0, refundService.updateRefund(refundDto));
+		} catch (ServiceException e) {
+			return new Response<Integer>(1, e.getMessage(), 0);
+		}
+	}
+
+	@RequestMapping(value = "/reopen", method = RequestMethod.GET)
+	@ResponseBody
+	public Response<Integer> reopenBrokerageSa(@RequestParam(value = "id") int id, HttpServletResponse response) {
+		try {
+			super.setGetHeader(response);
+			RefundDTO refundDto = refundService.getRefundById(id);
+			refundDto.setClose(false);
+			return new Response<Integer>(0, refundService.updateRefund(refundDto));
+		} catch (ServiceException e) {
+			return new Response<Integer>(1, e.getMessage(), 0);
+		}
+	}
 
 	@RequestMapping(value = "/delete", method = RequestMethod.GET)
 	@ResponseBody
