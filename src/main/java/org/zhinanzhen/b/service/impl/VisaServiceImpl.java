25ac115093a6a33ed0ba9fabf93447eb19066a33
Larry <larry7696@gmail.com>
2018-04-16 10:56:02 +0800

    visa service

---
+++ src/main/java/org/zhinanzhen/b/service/impl/VisaServiceImpl.java
@@ -0,0 +1,163 @@
+package org.zhinanzhen.b.service.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+import org.zhinanzhen.b.dao.VisaDAO;
+import org.zhinanzhen.b.dao.OfficialDAO;
+import org.zhinanzhen.b.dao.ReceiveTypeDAO;
+import org.zhinanzhen.b.dao.pojo.VisaDO;
+import org.zhinanzhen.b.dao.pojo.VisaListDO;
+import org.zhinanzhen.b.dao.pojo.OfficialDO;
+import org.zhinanzhen.b.dao.pojo.ReceiveTypeDO;
+import org.zhinanzhen.b.service.VisaService;
+import org.zhinanzhen.b.service.pojo.VisaDTO;
+import org.zhinanzhen.tb.dao.AdviserDAO;
+import org.zhinanzhen.tb.dao.pojo.AdviserDO;
+import org.zhinanzhen.tb.service.ServiceException;
+import org.zhinanzhen.tb.service.impl.BaseService;
+
+import com.ikasoa.core.thrift.ErrorCodeEnum;
+
+@Service("VisaService")
+public class VisaServiceImpl extends BaseService implements VisaService {
+
+	@Resource
+	private VisaDAO visaDao;
+
+	@Resource
+	private AdviserDAO adviserDao;
+
+	@Resource
+	private OfficialDAO officialDao;
+
+	@Resource
+	private ReceiveTypeDAO receiveTypeDao;
+
+	@Override
+	public int addVisa(VisaDTO visaDto) throws ServiceException {
+		if (visaDto == null) {
+			ServiceException se = new ServiceException("visaDto is null !");
+			se.setCode(ErrorCodeEnum.PARAMETER_ERROR.code());
+			throw se;
+		}
+		try {
+			VisaDO visaDo = mapper.map(visaDto, VisaDO.class);
+			if (visaDao.addVisa(visaDo) > 0) {
+				return visaDo.getId();
+			} else {
+				return 0;
+			}
+		} catch (Exception e) {
+			ServiceException se = new ServiceException(e);
+			se.setCode(ErrorCodeEnum.OTHER_ERROR.code());
+			throw se;
+		}
+	}
+
+	@Override
+	public int updateVisa(VisaDTO visaDto) throws ServiceException {
+		if (visaDto == null) {
+			ServiceException se = new ServiceException("visaDto is null !");
+			se.setCode(ErrorCodeEnum.PARAMETER_ERROR.code());
+			throw se;
+		}
+		try {
+			VisaDO visaDo = mapper.map(visaDto, VisaDO.class);
+			return visaDao.updateVisa(visaDo);
+		} catch (Exception e) {
+			ServiceException se = new ServiceException(e);
+			se.setCode(ErrorCodeEnum.OTHER_ERROR.code());
+			throw se;
+		}
+	}
+
+	@Override
+	public int countVisa(String keyword, String startHandlingDate, String endHandlingDate, String stardDate,
+			String endDate, Integer adviserId) throws ServiceException {
+		return visaDao.countVisa(keyword, startHandlingDate, endHandlingDate, stardDate, endDate, adviserId);
+	}
+
+	@Override
+	public List<VisaDTO> listVisa(String keyword, String startHandlingDate, String endHandlingDate, String stardDate,
+			String endDate, Integer adviserId, int pageNum, int pageSize) throws ServiceException {
+		if (pageNum < 0) {
+			pageNum = DEFAULT_PAGE_NUM;
+		}
+		if (pageSize < 0) {
+			pageSize = DEFAULT_PAGE_SIZE;
+		}
+		List<VisaDTO> visaDtoList = new ArrayList<>();
+		List<VisaListDO> visaListDoList = new ArrayList<>();
+		try {
+			visaListDoList = visaDao.listVisa(keyword, startHandlingDate, endHandlingDate, stardDate, endDate,
+					adviserId, pageNum * pageSize, pageSize);
+			if (visaListDoList == null) {
+				return null;
+			}
+		} catch (Exception e) {
+			ServiceException se = new ServiceException(e);
+			se.setCode(ErrorCodeEnum.EXECUTE_ERROR.code());
+			throw se;
+		}
+		for (VisaDO visaListDo : visaListDoList) {
+			VisaDTO visaDto = mapper.map(visaListDo, VisaDTO.class);
+			AdviserDO adviserDo = adviserDao.getAdviserById(visaListDo.getAdviserId());
+			if (adviserDo != null) {
+				visaDto.setAdviserName(adviserDo.getName());
+			}
+			OfficialDO officialDo = officialDao.getOfficialById(visaListDo.getOfficialId());
+			if (officialDo != null) {
+				visaDto.setOfficialName(officialDo.getName());
+			}
+			ReceiveTypeDO receiveTypeDo = receiveTypeDao.getReceiveTypeById(visaListDo.getReceiveTypeId());
+			if (receiveTypeDo != null) {
+				visaDto.setReceiveTypeName(receiveTypeDo.getName());
+			}
+			visaDtoList.add(visaDto);
+		}
+		return visaDtoList;
+	}
+
+	@Override
+	public VisaDTO getVisaById(int id) throws ServiceException {
+		if (id <= 0) {
+			ServiceException se = new ServiceException("id error !");
+			se.setCode(ErrorCodeEnum.PARAMETER_ERROR.code());
+			throw se;
+		}
+		VisaDTO visaDto = null;
+		try {
+			VisaDO visaDo = visaDao.getVisaById(id);
+			if (visaDo == null) {
+				return null;
+			}
+			visaDto = mapper.map(visaDo, VisaDTO.class);
+		} catch (Exception e) {
+			ServiceException se = new ServiceException(e);
+			se.setCode(ErrorCodeEnum.OTHER_ERROR.code());
+			throw se;
+		}
+		return visaDto;
+	}
+
+	@Override
+	public int deleteVisaById(int id) throws ServiceException {
+		if (id <= 0) {
+			ServiceException se = new ServiceException("id error !");
+			se.setCode(ErrorCodeEnum.PARAMETER_ERROR.code());
+			throw se;
+		}
+		try {
+			return visaDao.deleteVisaById(id);
+		} catch (Exception e) {
+			ServiceException se = new ServiceException(e);
+			se.setCode(ErrorCodeEnum.OTHER_ERROR.code());
+			throw se;
+		}
+	}
+
+}
