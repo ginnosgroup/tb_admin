d77aca81559d66335f4a7240dce4f814a805fc74
Larry <larry7696@gmail.com>
2018-11-21 10:46:47 +0800

    coding

--- src/main/java/org/zhinanzhen/tb/service/OrderService.java
+++ src/main/java/org/zhinanzhen/tb/service/OrderService.java
@@ -13,11 +13,14 @@ public interface OrderService {
 	public List<OrderDTO> listOrder(Integer id, String name, Integer regionId, OrderStateEnum state, String userName,
 			String userPhone, int pageNum, int pageSize) throws ServiceException;
 
+	public List<OrderDTO> listOrderBySubjectParentId(int subjectParentId) throws ServiceException;
+
 	public OrderDTO getOrderById(int id) throws ServiceException;
 
 	public boolean allocatingAdviser(int id, int adviserId) throws ServiceException;
-	
-	public List<OrderDTO> listOrder(int userId,String classify) throws ServiceException;
-	
-        boolean endingMoney(int orderId,double remainPayAmount,double remainPayBalance,Date remainPayDate) throws ServiceException;
+
+	public List<OrderDTO> listOrder(int userId, String classify) throws ServiceException;
+
+	boolean endingMoney(int orderId, double remainPayAmount, double remainPayBalance, Date remainPayDate)
+			throws ServiceException;
 }
