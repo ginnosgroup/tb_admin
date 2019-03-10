55bd5dde013b9c4fcaaad1d67b81a399d3e04855
Larry <larry7696@gmail.com>
2019-03-11 05:49:07 +0800

    consultation

--- src/main/java/org/zhinanzhen/tb/dao/ConsultationDAO.java
+++ src/main/java/org/zhinanzhen/tb/dao/ConsultationDAO.java
@@ -1,5 +1,16 @@
 package org.zhinanzhen.tb.dao;
 
+import java.util.Date;
+import java.util.List;
+
+import org.zhinanzhen.tb.dao.pojo.ConsultationDO;
+
 public interface ConsultationDAO {
+
+	public int addConsultation(ConsultationDO consultationDO);
+
+	public List<ConsultationDO> listConsultationByUserId(Integer userId);
+
+	public List<ConsultationDO> listConsultationByRemindDate(Date date);
 
 }
