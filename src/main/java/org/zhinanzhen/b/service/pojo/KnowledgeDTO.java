9841eb72ff8d9d457fb75c7c5bfe522e093d5bec
Larry <larry7696@gmail.com>
2019-06-23 14:47:02 +0800

    Knowledge(V2.2)

---
+++ src/main/java/org/zhinanzhen/b/service/pojo/KnowledgeDTO.java
@@ -0,0 +1,20 @@
+package org.zhinanzhen.b.service.pojo;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class KnowledgeDTO {
+
+	private int id;
+
+	private Date gmtCreate;
+
+	private String title;
+
+	private String content;
+
+	private int knowledgeMenuId;
+
+}
---
+++ src/main/java/org/zhinanzhen/b/service/pojo/KnowledgeMenuDTO.java
@@ -0,0 +1,14 @@
+package org.zhinanzhen.b.service.pojo;
+
+import lombok.Data;
+
+@Data
+public class KnowledgeMenuDTO {
+
+	private int id;
+
+	private String name;
+
+	private int parentId;
+
+}
