c86835b87d880b6b1421f006e221d03185e7ba63
Larry <larry7696@gmail.com>
2019-08-12 08:11:00 +0800

    school card

--- src/main/java/org/zhinanzhen/b/dao/SchoolSettingDAO.java
+++ src/main/java/org/zhinanzhen/b/dao/SchoolSettingDAO.java
@@ -1,5 +1,18 @@
 package org.zhinanzhen.b.dao;
 
+import java.util.Date;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import org.zhinanzhen.b.dao.pojo.SchoolSettingDO;
+
 public interface SchoolSettingDAO {
+
+	public int add(SchoolSettingDO schoolSettingDo);
+
+	public int update(@Param("id") int id, @Param("type") int type, @Param("startDate") Date startDate,
+			@Param("endDate") Date endDate, @Param("parameters") String parameters);
+
+	public List<SchoolSettingDO> list();
 
 }
--- src/main/java/org/zhinanzhen/b/dao/SubjectSettingDAO.java
+++ src/main/java/org/zhinanzhen/b/dao/SubjectSettingDAO.java
@@ -1,5 +1,16 @@
 package org.zhinanzhen.b.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import org.zhinanzhen.b.dao.pojo.SubjectSettingDO;
+
 public interface SubjectSettingDAO {
+
+	public int add(SubjectSettingDO subjectSettingDo);
+
+	public int update(@Param("id") int id, @Param("price") double price);
+
+	public List<SubjectSettingDO> list(@Param("schoolSettingId") int schoolSettingId);
 
 }
