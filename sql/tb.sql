4f682dbff0915d4c7959d60cd6de2c78d6801bed
Larry <larry7696@gmail.com>
2019-10-17 04:35:30 +0800

    tag

--- sql/tb.sql
+++ sql/tb.sql
@@ -453,3 +453,20 @@ CREATE TABLE `b_subject_setting` (
   `subject` varchar(128) NOT NULL COMMENT '课程',
   `price` decimal(8,2) NOT NULL DEFAULT '0.00' COMMENT '价格'
 ) ENGINE=InnoDB AUTO_INCREMENT=1000000 DEFAULT CHARSET=utf8;
+
+-- 客户标签关联
+CREATE TABLE `b_user_tag` (
+  `id` int PRIMARY KEY AUTO_INCREMENT NOT NULL COMMENT '编号',
+  `gmt_create` datetime NOT NULL COMMENT '创建时间',
+  `gmt_modify` datetime NOT NULL COMMENT '最后修改时间',
+  `user_id` int NOT NULL COMMENT '客户编号',
+  `tag_id` datetime NOT NULL COMMENT '标签编号'
+) ENGINE=InnoDB AUTO_INCREMENT=1000000 DEFAULT CHARSET=utf8;
+
+-- 标签
+CREATE TABLE `b_tag` (
+  `id` int PRIMARY KEY AUTO_INCREMENT NOT NULL COMMENT '编号',
+  `gmt_create` datetime NOT NULL COMMENT '创建时间',
+  `gmt_modify` datetime NOT NULL COMMENT '最后修改时间',
+  `name` varchar(32) NOT NULL COMMENT '名称'
+) ENGINE=InnoDB AUTO_INCREMENT=1000000 DEFAULT CHARSET=utf8;
