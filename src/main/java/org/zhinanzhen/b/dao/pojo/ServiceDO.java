bf397e4cb0787950cbbcdddb71e9b29b5620f6b9
Larry <larry7696@gmail.com>
2019-12-31 06:37:12 +0800

    service api

--- src/main/java/org/zhinanzhen/b/dao/pojo/ServiceDO.java
+++ src/main/java/org/zhinanzhen/b/dao/pojo/ServiceDO.java
@@ -1,12 +1,46 @@
 package org.zhinanzhen.b.dao.pojo;
 
+import java.util.Date;
+
 import lombok.Data;
 
 @Data
 public class ServiceDO {
 
 	private int id;
+	
+	private String type;
 
 	private String code;
+	
+	private boolean isPay;
+	
+	private int receiveTypeId;
+	
+	private Date receiveDate;
+	
+	private double receivable;
+	
+	private double discount;
+	
+	private double received;
+	
+	private int paymentTimes;
+	
+	private double amount;
+	
+	private double gst;
+	
+	private double deductGst;
+	
+	private double bonus;
+	
+	private int maraId;
+	
+	private int adviserId;
+	
+	private int officialId;
+	
+	private String remarks;
 
 }
