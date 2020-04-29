bfb79ba10c53871283adf668ce80db14b04092b4
Larry <larry7696@gmail.com>
2020-04-29 10:34:55 +0800

    修改备注字段类型

--- sql/tb.sql
+++ sql/tb.sql
@@ -209,7 +209,7 @@ CREATE TABLE `b_visa` (
   `adviser_id` int NOT NULL COMMENT '顾问编号 (对应tb_adviser.id)',
   `mara_id` int NOT NULL COMMENT '所属MARA编号 (对应b_mara.id)',
   `official_id` int NOT NULL COMMENT '文案编号 (对应b_official.id)',
-  `remarks` varchar(255) DEFAULT NULL COMMENT '备注',
+  `remarks` text DEFAULT NULL COMMENT '备注',
   `is_close` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否已取消'
 ) ENGINE=InnoDB AUTO_INCREMENT=1000000 DEFAULT CHARSET=utf8;
 
@@ -231,7 +231,7 @@ CREATE TABLE `b_brokerage` (
   `bonus` decimal(8,2) NOT NULL COMMENT '月奖金',
   `adviser_id` int NOT NULL COMMENT '顾问编号 (对应tb_adviser.id)',
   `official_id` int NOT NULL COMMENT '文案编号 (对应b_official.id)',
-  `remarks` varchar(255) DEFAULT NULL COMMENT '备注',
+  `remarks` text DEFAULT NULL COMMENT '备注',
   `is_close` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否已取消'
 ) ENGINE=InnoDB AUTO_INCREMENT=1000000 DEFAULT CHARSET=utf8;
 
@@ -254,7 +254,7 @@ CREATE TABLE `b_brokerage_sa` (
   `bonus` decimal(8,2) NOT NULL COMMENT '月奖金',
   `adviser_id` int NOT NULL COMMENT '顾问编号 (对应tb_adviser.id)',
   `official_id` int NOT NULL COMMENT '文案编号 (对应b_official.id)',
-  `remarks` varchar(255) DEFAULT NULL COMMENT '备注',
+  `remarks` text DEFAULT NULL COMMENT '备注',
   `is_close` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否已取消'
 ) ENGINE=InnoDB AUTO_INCREMENT=1000000 DEFAULT CHARSET=utf8;
 
@@ -282,7 +282,7 @@ CREATE TABLE `b_school_brokerage_sa` (
   `subagency_id` int DEFAULT NULL COMMENT '代理编号 (对应b_subagency.id)',
   `adviser_id` int NOT NULL COMMENT '顾问编号 (对应tb_adviser.id)',
   `official_id` int NOT NULL COMMENT '文案编号 (对应b_official.id)',
-  `remarks` varchar(255) DEFAULT NULL COMMENT '备注',
+  `remarks` text DEFAULT NULL COMMENT '备注',
   `is_settle_accounts` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否已结佣',
   `is_close` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否已取消'
 ) ENGINE=InnoDB AUTO_INCREMENT=1000000 DEFAULT CHARSET=utf8;
@@ -308,7 +308,7 @@ CREATE TABLE `b_refund` (
   `refund` decimal(8,2) NOT NULL COMMENT 'Refund',
   `adviser_id` int NOT NULL COMMENT '顾问编号 (对应tb_adviser.id)',
   `official_id` int NOT NULL COMMENT '文案编号 (对应b_official.id)',
-  `remarks` varchar(255) DEFAULT NULL COMMENT '备注',
+  `remarks` text DEFAULT NULL COMMENT '备注',
   `is_close` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否已取消'
 ) ENGINE=InnoDB AUTO_INCREMENT=1000000 DEFAULT CHARSET=utf8;
 
@@ -370,7 +370,7 @@ CREATE TABLE `b_service_order` (
   `mara_id` int DEFAULT NULL COMMENT '所属MARA编号 (对应b_mara.id,留学服务MARA为空)',
   `adviser_id` int NOT NULL COMMENT '顾问编号 (对应tb_adviser.id)',
   `official_id` int NOT NULL COMMENT '文案编号 (对应b_official.id)',
-  `remarks` varchar(255) DEFAULT NULL COMMENT '备注',
+  `remarks` text DEFAULT NULL COMMENT '备注',
   `closed_reason` varchar(255) DEFAULT NULL COMMENT '关闭原因',
   `is_delete` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否已删除'
 ) ENGINE=InnoDB AUTO_INCREMENT=1000000 DEFAULT CHARSET=utf8;
@@ -429,7 +429,7 @@ CREATE TABLE `b_commission_order` (
   `deduct_gst` decimal(8,2) NOT NULL COMMENT 'Deduct GST',
   `bonus` decimal(8,2) NOT NULL COMMENT '月奖金',
   `bonus_date` datetime DEFAULT NULL COMMENT '月奖金支付时间',
-  `remarks` varchar(255) DEFAULT NULL COMMENT '备注',
+  `remarks` text DEFAULT NULL COMMENT '备注',
   `is_close` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否已取消'
 ) ENGINE=InnoDB AUTO_INCREMENT=1000000 DEFAULT CHARSET=utf8;
 
