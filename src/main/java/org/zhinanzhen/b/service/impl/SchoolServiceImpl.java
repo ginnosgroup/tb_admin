2437383eeb6479a55026cfe46fd0295cf70397f9
Larry <larry7696@gmail.com>
2020-05-07 16:11:52 +0800

    debug

--- src/main/java/org/zhinanzhen/b/service/impl/SchoolServiceImpl.java
+++ src/main/java/org/zhinanzhen/b/service/impl/SchoolServiceImpl.java
@@ -234,14 +234,14 @@ public class SchoolServiceImpl extends BaseService implements SchoolService {
 			return null;
 		schoolDoList.forEach(schoolDo -> {
 			String name = schoolDo.getSubject();
-System.out.println("===== subject name is " + name);
-			SubjectSettingDO subjectSettingDo = subjectSettingDao.get(schoolSettingId, name);
-			if (subjectSettingDo == null) {
-				subjectSettingDo = new SubjectSettingDO();
-				subjectSettingDo.setSchoolSettingId(schoolSettingId);
-				subjectSettingDo.setSubject(name);
-System.out.println("===== add a subject " + subjectSettingDo);
-				subjectSettingDao.add(subjectSettingDo);
+			if (name != null) {
+				SubjectSettingDO subjectSettingDo = subjectSettingDao.get(schoolSettingId, name);
+				if (subjectSettingDo == null) {
+					subjectSettingDo = new SubjectSettingDO();
+					subjectSettingDo.setSchoolSettingId(schoolSettingId);
+					subjectSettingDo.setSubject(name);
+					subjectSettingDao.add(subjectSettingDo);
+				}
 			}
 		});
 		List<SubjectSettingDO> subjectSettingDoList = subjectSettingDao.list(schoolSettingId);
