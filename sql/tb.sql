491311218c0eabf9afc57f1cc3215f6e34216bf2
Larry <larry7696@gmail.com>
2020-05-11 12:29:46 +0800

    服务包逻辑删除

--- sql/tb.sql
+++ sql/tb.sql
@@ -329,7 +329,8 @@ CREATE TABLE `b_service_package` (
   `gmt_modify` datetime NOT NULL COMMENT '最后修改时间',
   `type` varchar(4) NOT NULL COMMENT '服务包类型(CA:职业评估,EOI:EOI,SA:学校申请,VA:签证申请)',
   `service_id` int DEFAULT NULL COMMENT '服务项目编号 (对应b_service.id)',
-  `num` int NOT NULL COMMENT '序号 (执行顺序)'
+  `num` int NOT NULL COMMENT '序号 (执行顺序)',
+  `is_delete` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否已删除'
 ) ENGINE=InnoDB AUTO_INCREMENT=1000000 DEFAULT CHARSET=utf8;
 
 -- 服务订单
