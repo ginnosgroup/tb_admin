4a3987eba4348d5d8098515ffc3ed9155989b785
Larry <larry7696@gmail.com>
2020-05-19 15:26:06 +0800

    提醒邮件

--- src/main/java/org/zhinanzhen/tb/utils/SendEmailUtil.java
+++ src/main/java/org/zhinanzhen/tb/utils/SendEmailUtil.java
@@ -2,17 +2,14 @@ package org.zhinanzhen.tb.utils;
 
 import com.ikasoa.web.utils.SimpleSendEmailTool;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 public class SendEmailUtil {
 
 	private static SimpleSendEmailTool simpleSendEmailTool = new SimpleSendEmailTool("notice@zhinanzhen.org",
 			"Znz@2020", SimpleSendEmailTool.SmtpServerEnum.EXMAIL_QQ);
 
 	public static void send(String mail, String title, String text) {
-		log.info("Send email to " + mail);
-		// simpleSendEmailTool.send(mail, title, text);
+		mail = "7311930@qq.com"; // 测试
+		simpleSendEmailTool.send(mail, title, text);
 	}
 
 }
