6b5b792385406e9e8ac62e45f7a93e526a6dd6ba
Larry <larry7696@gmail.com>
2020-05-26 12:20:11 +0800

    debug

--- src/main/java/org/zhinanzhen/tb/utils/SendEmailUtil.java
+++ src/main/java/org/zhinanzhen/tb/utils/SendEmailUtil.java
@@ -1,7 +1,5 @@
 package org.zhinanzhen.tb.utils;
 
-import com.ikasoa.web.utils.SimpleSendEmailTool;
-
 public class SendEmailUtil {
 
 	private static SimpleSendEmailTool simpleSendEmailTool = new SimpleSendEmailTool("notice@zhinanzhen.org",
@@ -9,6 +7,7 @@ public class SendEmailUtil {
 
 	public static void send(String mail, String title, String text) {
 		mail = "7311930@qq.com"; // 测试
+System.out.println("+++++ mail:" + mail + ", title:" + title + ", text:" + text);
 		simpleSendEmailTool.send(mail, title, text);
 	}
 
