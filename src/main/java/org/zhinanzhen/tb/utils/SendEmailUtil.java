47e64d012c127bfb4e3b37d67ecd740364025982
Larry <larry7696@gmail.com>
2020-06-03 09:49:22 +0800

    修改测试邮件地址

--- src/main/java/org/zhinanzhen/tb/utils/SendEmailUtil.java
+++ src/main/java/org/zhinanzhen/tb/utils/SendEmailUtil.java
@@ -11,7 +11,7 @@ public class SendEmailUtil {
 			"Znz@2020", SimpleSendEmailTool.SmtpServerEnum.EXMAIL_QQ);
 
 	public static void send(String mail, String title, String text) {
-//		mail = "7311930@qq.com"; // 测试
+		mail = "7311930@qq.com"; // 测试
 		try {
 			log.debug("发送邮件:" + mail + " | " + title);
 			simpleSendEmailTool.send(mail, title, text);
