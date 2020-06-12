bd095afd12ffb2cde6bae3c3efd22e8c271729a6
Larry <larry7696@gmail.com>
2020-06-12 15:26:10 +0800

    bugfix

--- src/main/java/org/zhinanzhen/tb/utils/SendEmailUtil.java
+++ src/main/java/org/zhinanzhen/tb/utils/SendEmailUtil.java
@@ -8,7 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 public class SendEmailUtil {
 
 	private static SimpleSendEmailTool simpleSendEmailTool = new SimpleSendEmailTool("notice@zhinanzhen.org",
-			"Znz@2020", SimpleSendEmailTool.SmtpServerEnum.EXMAIL_QQ);
+			"Znz@2020#IT", SimpleSendEmailTool.SmtpServerEnum.EXMAIL_QQ);
 
 	public static void send(String mail, String title, String text) {
 		mail = "7311930@qq.com"; // 测试
