906e29c2affdc4376ab614eda75cffc6ad7df3d4
Larry <larry7696@gmail.com>
2020-06-30 12:25:14 +0800

    bugfix

--- src/main/java/org/zhinanzhen/tb/service/impl/AdminUserServiceImpl.java
+++ src/main/java/org/zhinanzhen/tb/service/impl/AdminUserServiceImpl.java
@@ -21,7 +21,7 @@ public class AdminUserServiceImpl extends BaseService implements AdminUserServic
 
 	@Override
 	public int add(String username, String password, String apList, Integer adviserId, Integer maraId,
-			Integer officialId) throws ServiceException {
+			Integer officialId, Integer kjId) throws ServiceException {
 		if (StringUtil.isEmpty(password)) {
 			ServiceException se = new ServiceException("password is null !");
 			se.setCode(ErrorCodeEnum.PARAMETER_ERROR.code());
@@ -38,6 +38,8 @@ public class AdminUserServiceImpl extends BaseService implements AdminUserServic
 				adminUserDo.setMaraId(maraId);
 			if (officialId != null)
 				adminUserDo.setOfficialId(officialId);
+			if (kjId != null)
+				adminUserDo.setKjId(kjId);
 			return adminUserDao.add(adminUserDo);
 		} catch (Exception e) {
 			ServiceException se = new ServiceException(e);
