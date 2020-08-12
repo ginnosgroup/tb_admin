88c191dd3a63ef63380a3f83160813fb00a63b92
BlueGuggling <59388880+BlueGuggling@users.noreply.github.com>
2020-08-12 14:52:04 +0800

    Update CommissionOrderController.java

--- src/main/java/org/zhinanzhen/b/controller/CommissionOrderController.java
+++ src/main/java/org/zhinanzhen/b/controller/CommissionOrderController.java
@@ -783,7 +783,7 @@ public class CommissionOrderController extends BaseCommissionOrderController {
 			return new Response<List<CommissionOrderListDTO>>(0,
 					commissionOrderService.listCommissionOrder(regionId, maraId, adviserId, officialId, userId, name,
 							phone, wechatUsername, schoolId, isSettle, stateList, commissionStateList,
-							startKjApprovalDate, endKjApprovalDate, isYzyAndYjy, pageNum, pageSize));
+							startKjApprovalDate, endKjApprovalDate, isYzyAndYjy,state, pageNum, pageSize));
 		} catch (ServiceException e) {
 			return new Response<List<CommissionOrderListDTO>>(1, e.getMessage(), null);
 		}
@@ -911,7 +911,7 @@ public class CommissionOrderController extends BaseCommissionOrderController {
 
 			List<CommissionOrderListDTO> commissionOrderList = commissionOrderService.listCommissionOrder(regionId,
 					maraId, adviserId, officialId, userId, name, phone, wechatUsername, schoolId, isSettle, stateList,
-					commissionStateList, startKjApprovalDate, endKjApprovalDate, isYzyAndYjy, 0, 9999);
+					commissionStateList, startKjApprovalDate, endKjApprovalDate, isYzyAndYjy,state, 0, 9999);
 
 			OutputStream os = response.getOutputStream();
 			jxl.Workbook wb;
