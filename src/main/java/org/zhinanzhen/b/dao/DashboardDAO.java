5c7911bcaf0116ac1eb4c6a199d19b68a91ba834
BlueGuggling <59388880+BlueGuggling@users.noreply.github.com>
2020-09-18 11:50:44 +0800

    dubug

--- src/main/java/org/zhinanzhen/b/dao/DashboardDAO.java
+++ src/main/java/org/zhinanzhen/b/dao/DashboardDAO.java
@@ -4,6 +4,8 @@ import org.apache.ibatis.annotations.Param;
 
 public interface DashboardDAO {
 
-    public Double getThisMonthExpectAmount(@Param("adviserId") Integer adviserId);
+    public Double getThisMonthVisaExpectAmount(@Param("adviserId") Integer adviserId);
+
+    public Double getThisMonthbCommissionOrderExpectAmountSBBTM(@Param("adviserId") Integer adviserId);
 
 }
