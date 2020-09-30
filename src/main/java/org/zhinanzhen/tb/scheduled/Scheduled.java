1b8b64ac82d77fb2bc445415db3f67c2fe63c2c7
MrBaomini <62210201+MrBaomini@users.noreply.github.com>
2020-09-30 12:25:57 +0800

    Update Scheduled.java

--- src/main/java/org/zhinanzhen/tb/scheduled/Scheduled.java
+++ src/main/java/org/zhinanzhen/tb/scheduled/Scheduled.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Component;
-import org.zhinanzhen.b.controller.TestController;
 import org.zhinanzhen.b.service.pojo.DataDTO;
 import org.zhinanzhen.tb.utils.SendEmailUtil;
 
