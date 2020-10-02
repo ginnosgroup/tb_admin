1ebc4d9e2444f4f285a96280761e5aff88eb6f65
Larry <larry7696@gmail.com>
2020-10-02 08:25:16 +0800

    文案标签

--- src/main/java/org/zhinanzhen/b/controller/OfficialTagController.java
+++ src/main/java/org/zhinanzhen/b/controller/OfficialTagController.java
@@ -20,10 +20,10 @@ import org.zhinanzhen.tb.service.ServiceException;
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/officialTag")
 public class OfficialTagController extends BaseController {
-	
+
 	@Resource
 	OfficialTagService officialTagService;
-	
+
 	@RequestMapping(value = "/addOfficialTag", method = RequestMethod.POST)
 	@ResponseBody
 	public Response<Integer> addOfficialTag(@RequestParam(value = "name") String name,
@@ -44,6 +44,55 @@ public class OfficialTagController extends BaseController {
 				return new Response<Integer>(1, "创建失败.", 0);
 		} catch (ServiceException e) {
 			return new Response<Integer>(e.getCode(), e.getMessage(), 0);
+		}
+	}
+
+	@RequestMapping(value = "/updateOfficialTag", method = RequestMethod.POST)
+	@ResponseBody
+	public Response<Integer> updateOfficialTag(@RequestParam(value = "id") Integer id,
+			@RequestParam(value = "name") String name, @RequestParam(value = "colour") String colour,
+			HttpServletRequest request, HttpServletResponse response) {
+		AdminUserLoginInfo adminUserLoginInfo = getAdminUserLoginInfo(request);
+		if (adminUserLoginInfo != null)
+			if (adminUserLoginInfo == null || !"WA".equalsIgnoreCase(adminUserLoginInfo.getApList())
+					|| adminUserLoginInfo.getOfficialId() == null)
+				return new Response<Integer>(1, "仅限文案操作.", null);
+		try {
+			super.setPostHeader(response);
+			OfficialTagDTO officialTagDto = officialTagService.get(id);
+			if (officialTagDto == null)
+				return new Response<Integer>(1, "没有找到该标签:" + id, null);
+			officialTagDto.setName(name);
+			officialTagDto.setColour(colour);
+			if (officialTagService.update(officialTagDto) > 0)
+				return new Response<Integer>(0, officialTagDto.getId());
+			else
+				return new Response<Integer>(1, "修改失败.", 0);
+		} catch (ServiceException e) {
+			return new Response<Integer>(e.getCode(), e.getMessage(), 0);
+		}
+	}
+
+	@RequestMapping(value = "/deleteOfficialTag", method = RequestMethod.POST)
+	@ResponseBody
+	public Response<Boolean> deleteOfficialTag(@RequestParam(value = "id") Integer id,
+			@RequestParam(value = "name") String name, @RequestParam(value = "colour") String colour,
+			HttpServletRequest request, HttpServletResponse response) {
+		AdminUserLoginInfo adminUserLoginInfo = getAdminUserLoginInfo(request);
+		if (adminUserLoginInfo != null)
+			if (adminUserLoginInfo == null || !"WA".equalsIgnoreCase(adminUserLoginInfo.getApList())
+					|| adminUserLoginInfo.getOfficialId() == null)
+				return new Response<Boolean>(1, "仅限文案操作.", false);
+		try {
+			super.setPostHeader(response);
+			if (officialTagService.get(id) == null)
+				return new Response<Boolean>(1, "没有找到该标签:" + id, false);
+			if (officialTagService.delete(id) > 0)
+				return new Response<Boolean>(0, true);
+			else
+				return new Response<Boolean>(1, "删除失败.", false);
+		} catch (ServiceException e) {
+			return new Response<Boolean>(e.getCode(), e.getMessage(), false);
 		}
 	}
 
