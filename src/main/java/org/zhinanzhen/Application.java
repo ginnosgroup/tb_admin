23489c00ef3f38bb28176ffc48777f65353247d4
Larry <larry7696@gmail.com>
2020-10-26 13:21:58 +0800

    springboot

---
+++ src/main/java/org/zhinanzhen/Application.java
@@ -0,0 +1,30 @@
+package org.zhinanzhen;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@MapperScan({ "org.zhinanzhen.tb.dao", "org.zhinanzhen.b.dao" })
+public class Application extends WebMvcConfigurerAdapter {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedOrigins("*");
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/webroot/**").addResourceLocations("classpath:/webroot/");
+	}
+
+}
