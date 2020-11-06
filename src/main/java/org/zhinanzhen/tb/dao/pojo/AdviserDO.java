f527ec88fec1ecacca18db2fbaac23652707c38f
Larry <larry7696@gmail.com>
2020-11-06 12:35:24 +0800

    性能优化

--- src/main/java/org/zhinanzhen/tb/dao/pojo/AdviserDO.java
+++ src/main/java/org/zhinanzhen/tb/dao/pojo/AdviserDO.java
@@ -1,14 +1,17 @@
 package org.zhinanzhen.tb.dao.pojo;
 
+import java.io.Serializable;
 import java.util.Date;
 
 import lombok.Data;
 
 @Data
-public class AdviserDO {
+public class AdviserDO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 	private int id;
-	
+
 	private Date gmtCreate;
 
 	private String name;
@@ -16,9 +19,9 @@ public class AdviserDO {
 	private String phone;
 
 	private String email;
-	
+
 	private String state;
-	
+
 	private String imageUrl;
 
 	private int regionId;
--- src/main/java/org/zhinanzhen/tb/dao/pojo/UserDO.java
+++ src/main/java/org/zhinanzhen/tb/dao/pojo/UserDO.java
@@ -1,11 +1,14 @@
 package org.zhinanzhen.tb.dao.pojo;
 
+import java.io.Serializable;
 import java.util.Date;
 
 import lombok.Data;
 
 @Data
-public class UserDO {
+public class UserDO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 	private int id;
 
