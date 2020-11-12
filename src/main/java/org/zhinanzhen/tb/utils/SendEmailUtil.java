53e484b3cffdf76754bfca145d050b14e61f92c1
Larry <larry7696@gmail.com>
2020-11-12 13:00:36 +0800

    性能优化

--- src/main/java/org/zhinanzhen/tb/utils/SendEmailUtil.java
+++ src/main/java/org/zhinanzhen/tb/utils/SendEmailUtil.java
@@ -12,12 +12,19 @@ public class SendEmailUtil {
 
 	public static void send(String mail, String title, String text) {
 //		mail = "7311930@qq.com"; // 测试
-		try {
-			log.info("发送邮件:" + mail + " | " + title + "|" + text);
-			simpleSendEmailTool.send(mail, title, text);
-		} catch (Exception e) {
-			log.error(e.getMessage());
-		}
+		log.info("发送邮件:" + mail + " | " + title + "|" + text);
+		Thread thread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					simpleSendEmailTool.send(mail, title, text);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		thread.start();
+		log.info("发送邮件完成:" + mail);
 	}
 
 }
