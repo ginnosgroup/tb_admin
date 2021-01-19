664eff3067a6657fd326c1d40f7657037d97f552
Larry <larry7696@gmail.com>
2021-01-19 10:37:56 +0800

    debug

--- src/main/java/org/zhinanzhen/b/controller/nodes/ServiceOrderOfficialReviewNode.java
+++ src/main/java/org/zhinanzhen/b/controller/nodes/ServiceOrderOfficialReviewNode.java
@@ -51,7 +51,7 @@ public class ServiceOrderOfficialReviewNode extends SODecisionNode {
 			context.putParameter("response", new Response<ServiceOrderDTO>(1, "只有签证类才能进行mara审核流程.", null));
 			return SUSPEND_NODE;
 		}
-		if (state == null) {
+		if (state == null && context.getParameter("state") == null) {
 			context.putParameter("response", new Response<ServiceOrderDTO>(1, "状态值不能为空.", null));
 			return SUSPEND_NODE;
 		}
--- src/main/java/org/zhinanzhen/b/controller/nodes/ServiceOrderReviewNode.java
+++ src/main/java/org/zhinanzhen/b/controller/nodes/ServiceOrderReviewNode.java
@@ -46,13 +46,17 @@ public class ServiceOrderReviewNode extends SODecisionNode {
 			context.putParameter("response", new Response<ServiceOrderDTO>(1, "服务订单执行异常:" + e.getMessage(), null));
 			return SUSPEND_NODE;
 		}
+System.out.println("context.state:" + context.getParameter("state"));
 		String state = getState(context, "OREVIEW", "WAIT", "CLOSE", "FINISH");
+System.out.println("state:" + state);
 		if ("WAIT".equalsIgnoreCase(state) && !"VISA".equalsIgnoreCase(getType(context))) {
 			context.putParameter("response", new Response<ServiceOrderDTO>(1, "只有签证类才能进行mara审核流程.", null));
 			return SUSPEND_NODE;
 		}
-		if (state == null)
+		if (state == null && context.getParameter("state") == null) {
+			context.putParameter("response", new Response<ServiceOrderDTO>(1, "状态值不能为空.", null));
 			return SUSPEND_NODE;
+		}
 		return state;
 	}
 
