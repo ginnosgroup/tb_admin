577f113853f3678e62f52a202f1b9c3ec904620c
Larry <larry7696@gmail.com>
2021-05-05 12:49:06 +0800

    mail log interface

--- src/main/java/org/zhinanzhen/b/service/MailLogService.java
+++ src/main/java/org/zhinanzhen/b/service/MailLogService.java
@@ -3,11 +3,12 @@ package org.zhinanzhen.b.service;
 import java.util.List;
 
 import org.zhinanzhen.b.service.pojo.MailLogDTO;
+import org.zhinanzhen.tb.service.ServiceException;
 
 public interface MailLogService {
 
-	int countMailLog();
+	int countMailLog() throws ServiceException;
 
-	List<MailLogDTO> listMailLog(int pageNum, int pageSize);
+	List<MailLogDTO> listMailLog(int pageNum, int pageSize) throws ServiceException;
 
 }
