10f277f61711481a1859eb1164e1082abe9d7995
Larry <larry7696@gmail.com>
2021-05-05 12:53:21 +0800

    mail log interface

---
+++ src/main/java/org/zhinanzhen/b/controller/MailLogController.java
@@ -0,0 +1,41 @@
+package org.zhinanzhen.b.controller;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.zhinanzhen.b.service.MailLogService;
+import org.zhinanzhen.b.service.pojo.MailLogDTO;
+import org.zhinanzhen.tb.controller.BaseController;
+import org.zhinanzhen.tb.controller.ListResponse;
+import org.zhinanzhen.tb.service.ServiceException;
+
+@Controller
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/mailLog")
+public class MailLogController extends BaseController {
+
+	@Resource
+	MailLogService mailLogService;
+
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	@ResponseBody
+	public ListResponse<List<MailLogDTO>> list(@RequestParam(value = "pageNum") int pageNum,
+			@RequestParam(value = "pageSize") int pageSize, HttpServletResponse response) {
+		try {
+			super.setGetHeader(response);
+			return new ListResponse<List<MailLogDTO>>(true, pageSize, mailLogService.countMailLog(),
+					mailLogService.listMailLog(pageNum, pageSize), "");
+		} catch (ServiceException e) {
+			return new ListResponse<List<MailLogDTO>>(false, pageSize, 0, null, e.getMessage());
+		}
+	}
+
+}
