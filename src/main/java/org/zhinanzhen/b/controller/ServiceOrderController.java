788e7510a88ad45b267ab2a84edea12e483457b7
Larry <larry7696@gmail.com>
2021-07-27 10:45:13 +0800

    debug

--- src/main/java/org/zhinanzhen/b/controller/ServiceOrderController.java
+++ src/main/java/org/zhinanzhen/b/controller/ServiceOrderController.java
@@ -922,7 +922,8 @@ public class ServiceOrderController extends BaseController {
 			if (id != null && id > 0) {
 				List<ServiceOrderDTO> list = new ArrayList<ServiceOrderDTO>();
 				ServiceOrderDTO serviceOrder = serviceOrderService.getServiceOrderById(id);
-				if ((serviceOrder != null && serviceOrder.getAdviserId() == adviserId) || isSuperAdminUser(request))
+				if ((serviceOrder != null && (adviserId != null && serviceOrder.getAdviserId() == adviserId))
+						|| isSuperAdminUser(request))
 					list.add(serviceOrder);
 				return new ListResponse<List<ServiceOrderDTO>>(true, pageSize, list.size(), list, "");
 			}
