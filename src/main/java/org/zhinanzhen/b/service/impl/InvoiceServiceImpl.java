2ca498ce2d7e2f9cf3d8f3665ab3096d7e9d1438
815124560@qq.com <baoshanzhou>
2021-09-01 18:02:55 +0800

    bugfix
    (cherry picked from commit 7e5ed58b91ce4d76e0168cf5216191fca1d7ecc0)

--- src/main/java/org/zhinanzhen/b/service/impl/InvoiceServiceImpl.java
+++ src/main/java/org/zhinanzhen/b/service/impl/InvoiceServiceImpl.java
@@ -586,7 +586,7 @@ public class InvoiceServiceImpl extends BaseService implements InvoiceService {
         }
         for (InvoiceSchoolDescriptionDO invoiceSchoolDescriptionDO : _description)  {
             if (invoiceSchoolDescriptionDO.getInstallmentDueDate() == null || ymdsdf.format(invoiceSchoolDescriptionDO.getInstallmentDueDate()).equalsIgnoreCase(STR1900)){
-                throw  new ServiceException("installment due date 时间错误!") ;
+                //throw  new ServiceException("installment due date 时间错误!") ;
             }
         }
     }
