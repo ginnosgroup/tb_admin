4194a0994c0ae7b4e22ff63f2b73c9b1bca27757
Larry <larry7696@gmail.com>
2021-11-25 06:23:34 +0800

    test

---
+++ src/main/java/org/zhinanzhen/b/controller/JianguoyunDemoController.java
@@ -0,0 +1,34 @@
+package org.zhinanzhen.b.controller;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.zhinanzhen.tb.controller.BaseController;
+import org.zhinanzhen.tb.controller.Response;
+
+/**
+ * 坚果云上传文件测试
+ */
+@Controller
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/jianguoyun")
+public class JianguoyunDemoController extends BaseController {
+	
+	@RequestMapping(value = "/upload", method = RequestMethod.POST)
+	@ResponseBody
+	public Response<String> uploadImage(@RequestParam MultipartFile file, HttpServletRequest request,
+			HttpServletResponse response) throws IllegalStateException, IOException {
+		super.setPostHeader(response);
+		return super.upload2(file, request.getSession(), "/uploads/jianguoyun_webdev/userfiles/");
+	}
+
+}
