d0ca18751c18f2dd31e1feb5929270df9f15a415
815124560@qq.com <baoshanzhou>
2021-11-24 17:36:20 +0800

    服务订单 downExcel兼容新学校库

--- src/main/java/org/zhinanzhen/b/dao/pojo/EachRegionNumberDO.java
+++ src/main/java/org/zhinanzhen/b/dao/pojo/EachRegionNumberDO.java
@@ -14,9 +14,27 @@ public class EachRegionNumberDO {
 
     private  int count;
 
+    /**
+     * 顾问地区name
+     */
     private String name;
 
+    /**
+     * OVST:学校名字  VISA:服务code
+     */
     private String code;
 
+    /**
+     * OVST:旧学校库存学校id,新学校库存课程id  VISA:b_service.id
+     */
     private String serviceId;
+
+    /**
+     * 下面这三个Name意思是新学校库学校name
+     */
+    private String instiName;
+
+    private String institutionTradingName;
+
+    private String institutionName;
 }
