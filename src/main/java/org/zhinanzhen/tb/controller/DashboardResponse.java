2e5885f23de12ddbcac2822a7b7bf3a1818d19d6
815124560@qq.com <baoshanzhou>
2021-12-20 11:36:36 +0800

    服务订单  导出服务订单申请时间列调整到前面

---
+++ src/main/java/org/zhinanzhen/tb/controller/DashboardResponse.java
@@ -0,0 +1,56 @@
+package org.zhinanzhen.tb.controller;
+
+import lombok.Data;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Date: 2021/12/20 上午 11:02
+ * Description:仪表板 图标展示数据API接口返回对象
+ * Version: V1.0
+ */
+@Data
+public class DashboardResponse<T> {
+    /**
+     * 编码
+     */
+    private int code;
+
+    /**
+     * 信息
+     */
+    private String message;
+
+    /**
+     * 数据
+     */
+    private T data;
+
+    /**
+     * 开始时间
+     */
+    private String dateOne;
+
+    /**
+     * 结束时间
+     */
+    private String dateTwo;
+
+    public DashboardResponse(int code, String message, T data, String dateOne, String dateTwo) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+        this.dateOne = dateOne;
+        this.dateTwo = dateTwo;
+    }
+
+    public DashboardResponse(int code, String message, T data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+
+    public DashboardResponse(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+}
