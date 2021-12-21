86eb5e482a3ee384ba3d83584e14511ae8702626
Larry <larry7696@gmail.com>
2021-12-22 06:09:45 +0800

    refund

--- src/main/java/org/zhinanzhen/b/service/pojo/RefundDTO.java
+++ src/main/java/org/zhinanzhen/b/service/pojo/RefundDTO.java
@@ -1,62 +1,9 @@
 package org.zhinanzhen.b.service.pojo;
 
-import java.util.Date;
+import org.zhinanzhen.b.dao.pojo.ManualRefundDO;
 
-import lombok.Data;
+public class ManualRefundDTO extends ManualRefundDO {
 
-@Data
-public class RefundDTO {
-
-	private int id;
-
-	private Date gmtCreate;
-
-	private Date handlingDate;
-
-	private int userId;
-
-	private String name;
-
-	private String userName;
-
-	private Date birthday;
-
-	private String phone;
-
-	private int receiveTypeId;
-
-	private String receiveTypeName;
-
-	private double amount;
-
-	private double preRefundAmount;
-
-	private String bankName;
-
-	private String bankAccount;
-
-	private String bsb;
-
-	private Date refundDate;
-
-	private double refundAmount;
-
-	private double gst;
-
-	private double deductGst;
-
-	private double refund;
-
-	private int adviserId;
-
-	private String adviserName;
-
-	private int officialId;
-
-	private String officialName;
-	
-	private String remarks;
-
-	private boolean isClose;
+	private static final long serialVersionUID = 1L;
 
 }
