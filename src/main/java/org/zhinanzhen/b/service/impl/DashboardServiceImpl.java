c5f78ab884c421934bf6094dd009e05a78c85578
815124560@qq.com <baoshanzhou>
2021-12-21 13:01:36 +0800

    dashboard  后续接口补充

--- src/main/java/org/zhinanzhen/b/service/impl/DashboardServiceImpl.java
+++ src/main/java/org/zhinanzhen/b/service/impl/DashboardServiceImpl.java
@@ -8,6 +8,8 @@ import org.zhinanzhen.b.service.DashboardService;
 import org.zhinanzhen.tb.service.ServiceException;
 import org.zhinanzhen.tb.service.impl.BaseService;
 
+import java.util.List;
+
 @Service("DashboardService")
 public class DashboardServiceImpl extends BaseService implements DashboardService {
 
@@ -18,9 +20,9 @@ public class DashboardServiceImpl extends BaseService implements DashboardServic
 	private DashboardDAO dashboardDAO;
 
 	@Override
-	public double getThisMonthExpectAmount(Integer adviserId) throws ServiceException {
-		Double visaExpectAmountSBBTM = dashboardDAO.getThisMonthVisaExpectAmount(adviserId);
-		Double bCommissionOrderExpectAmountSBBTM = dashboardDAO.getThisMonthbCommissionOrderExpectAmountSBBTM(adviserId);
+	public double getThisMonthExpectAmount(Integer adviserId, List<Integer> regionIdList) throws ServiceException {
+		Double visaExpectAmountSBBTM = dashboardDAO.getThisMonthVisaExpectAmount(adviserId, regionIdList);
+		Double bCommissionOrderExpectAmountSBBTM = dashboardDAO.getThisMonthbCommissionOrderExpectAmountSBBTM(adviserId, regionIdList);
 
 
 		return (visaExpectAmountSBBTM == null ? 0.00 : visaExpectAmountSBBTM) +
