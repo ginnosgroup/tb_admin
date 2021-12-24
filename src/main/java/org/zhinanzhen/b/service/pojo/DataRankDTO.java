04fad002bd854c23273abf1f286bb5098fd865c5
815124560@qq.com <baoshanzhou>
2021-12-24 16:11:51 +0800

    dashboard Rank排名

---
+++ src/main/java/org/zhinanzhen/b/service/pojo/DataRankDTO.java
@@ -0,0 +1,16 @@
+package org.zhinanzhen.b.service.pojo;
+
+import lombok.Data;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Date: 2021/12/24 下午 2:29
+ * Description:
+ * Version: V1.0
+ */
+@Data
+public class DataRankDTO extends DataDTO{
+
+    private int rank;
+
+}
