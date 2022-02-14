b2f533292bb8664387c45c8a7f1165f3ed5a7b86
815124560@qq.com <baoshanzhou>
2022-02-14 16:20:32 +0800

    退款数据整合到report

---
+++ src/main/java/org/zhinanzhen/b/service/pojo/RefoundReportDTO.java
@@ -0,0 +1,12 @@
+package org.zhinanzhen.b.service.pojo;
+
+import org.zhinanzhen.b.dao.pojo.RefoundReportDO;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Date: 2022/02/9 下午 11:06
+ * Description:
+ * Version: V1.0
+ */
+public class RefoundReportDTO extends RefoundReportDO {
+}
