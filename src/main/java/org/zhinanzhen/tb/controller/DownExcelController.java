d1297e8d9033c22811403c614ca50951410cb4de
Larry <larry7696@gmail.com>
2022-04-07 15:59:00 +0800

    test

--- src/main/java/org/zhinanzhen/tb/controller/DownExcelController.java
+++ src/main/java/org/zhinanzhen/tb/controller/DownExcelController.java
@@ -397,14 +397,14 @@ public class DownExcelController extends BaseController {
 				adviserIdList = Arrays.asList(adviserIds.split(","));
 
 			response.reset();// 清空输出流
-			//String tableName = "commission_report_information";
-			//response.setHeader("Content-disposition",
-			//		"attachment; filename=" + new String(tableName.getBytes("GB2312"), "8859_1") + ".xls");
-			//response.setContentType("application/msexcel");
-			String tableName = "压缩包";
+			String tableName = "commission_report_information";
 			response.setHeader("Content-disposition",
-					"attachment; filename=" + new String(tableName.getBytes("GB2312"), "8859_1") + ".zip");
-			response.setContentType("application/zip");
+					"attachment; filename=" + new String(tableName.getBytes("GB2312"), "8859_1") + ".xls");
+			response.setContentType("application/msexcel");
+//			String tableName = "压缩包";
+//			response.setHeader("Content-disposition",
+//					"attachment; filename=" + new String(tableName.getBytes("GB2312"), "8859_1") + ".zip");
+//			response.setContentType("application/zip");
 
 			Map<String, CommissionReport> crMap = new HashMap<>();
 			List<VisaReportDTO> visaReportList = visaService.listVisaReport(startDate, endDate, dateType, dateMethod,
