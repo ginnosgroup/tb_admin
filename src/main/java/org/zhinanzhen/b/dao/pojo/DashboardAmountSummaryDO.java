a3ba80a96e6bbeec0d785400b7cd77978e19a013
Larry <larry7696@gmail.com>
2022-04-25 11:59:31 +0800

    bugfix

--- src/main/java/org/zhinanzhen/b/dao/pojo/DashboardAmountSummaryDO.java
+++ src/main/java/org/zhinanzhen/b/dao/pojo/DashboardAmountSummaryDO.java
@@ -1,14 +1,18 @@
 package org.zhinanzhen.b.dao.pojo;
 
+import java.io.Serializable;
+
 import lombok.Data;
 
 @Data
-public class DashboardAmountSummaryDO {
+public class DashboardAmountSummaryDO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 	private int id;
 
 	private String name1;
-	
+
 	private String name2;
 
 	private int total;
