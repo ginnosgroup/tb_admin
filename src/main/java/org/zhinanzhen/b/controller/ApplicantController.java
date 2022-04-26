3d8227fd4b03c03e58421d8699cab43ce3fab692
Larry <larry7696@gmail.com>
2022-04-26 20:17:35 +0800

    申请人

---
+++ src/main/java/org/zhinanzhen/b/controller/ApplicantController.java
@@ -0,0 +1,160 @@
+package org.zhinanzhen.b.controller;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.zhinanzhen.b.service.ApplicantService;
+import org.zhinanzhen.b.service.pojo.ApplicantDTO;
+import org.zhinanzhen.tb.controller.BaseController;
+import org.zhinanzhen.tb.controller.ListResponse;
+import org.zhinanzhen.tb.controller.Response;
+import org.zhinanzhen.tb.service.ServiceException;
+
+import com.ikasoa.core.utils.StringUtil;
+
+@Controller
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/applicant")
+public class ApplicantController extends BaseController {
+
+	@Resource
+	ApplicantService applicantService;
+
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	@ResponseBody
+	public Response<Integer> add(@RequestParam(value = "surname") String surname,
+			@RequestParam(value = "firstname") String firstname, @RequestParam(value = "birthday") String birthday,
+			@RequestParam(value = "type") String type, @RequestParam(value = "visaCode") String visaCode,
+			@RequestParam(value = "visaExpirationDate") String visaExpirationDate,
+			@RequestParam(value = "nutCloud", required = false) String nutCloud,
+			@RequestParam(value = "fileUrl", required = false) String fileUrl,
+			@RequestParam(value = "firstControllerContents", required = false) String firstControllerContents,
+			@RequestParam(value = "userId") Integer userId, HttpServletRequest request, HttpServletResponse response) {
+		try {
+			super.setPostHeader(response);
+			ApplicantDTO applicantDto = new ApplicantDTO();
+			applicantDto.setSurname(surname);
+			applicantDto.setFirstname(firstname);
+			applicantDto.setType(type);
+			applicantDto.setVisaCode(visaCode);
+			applicantDto.setVisaExpirationDate(new Date(Long.parseLong(visaExpirationDate)));
+			if (StringUtil.isNotEmpty(nutCloud))
+				applicantDto.setNutCloud(nutCloud);
+			if (StringUtil.isNotEmpty(fileUrl))
+				applicantDto.setFileUrl(fileUrl);
+			if (StringUtil.isNotEmpty(firstControllerContents))
+				applicantDto.setFirstControllerContents(firstControllerContents);
+			applicantDto.setUserId(userId);
+			if (applicantService.add(applicantDto) > 0) {
+				return new Response<Integer>(0, applicantDto.getId());
+			} else {
+				return new Response<Integer>(1, "创建失败.", 0);
+			}
+		} catch (ServiceException e) {
+			return new Response<Integer>(e.getCode(), e.getMessage(), 0);
+		}
+	}
+
+	@RequestMapping(value = "/update", method = RequestMethod.PUT)
+	@ResponseBody
+	public Response<Integer> update(@RequestParam(value = "id") int id,
+			@RequestParam(value = "surname", required = false) String surname,
+			@RequestParam(value = "firstname", required = false) String firstname,
+			@RequestParam(value = "birthday", required = false) String birthday,
+			@RequestParam(value = "type", required = false) String type,
+			@RequestParam(value = "visaCode", required = false) String visaCode,
+			@RequestParam(value = "visaExpirationDate", required = false) String visaExpirationDate,
+			@RequestParam(value = "nutCloud", required = false) String nutCloud,
+			@RequestParam(value = "fileUrl", required = false) String fileUrl,
+			@RequestParam(value = "firstControllerContents", required = false) String firstControllerContents,
+			@RequestParam(value = "userId", required = false) Integer userId, HttpServletRequest request,
+			HttpServletResponse response) {
+		try {
+			super.setPostHeader(response);
+			ApplicantDTO applicantDto = applicantService.getById(id);
+			if (StringUtil.isNotEmpty(surname))
+				applicantDto.setSurname(surname);
+			if (StringUtil.isNotEmpty(firstname))
+				applicantDto.setFirstname(firstname);
+			if (StringUtil.isNotEmpty(type))
+				applicantDto.setType(type);
+			if (StringUtil.isNotEmpty(visaCode))
+				applicantDto.setVisaCode(visaCode);
+			if (StringUtil.isNotEmpty(visaExpirationDate))
+				applicantDto.setVisaExpirationDate(new Date(Long.parseLong(visaExpirationDate)));
+			if (StringUtil.isNotEmpty(nutCloud))
+				applicantDto.setNutCloud(nutCloud);
+			if (StringUtil.isNotEmpty(fileUrl))
+				applicantDto.setFileUrl(fileUrl);
+			if (StringUtil.isNotEmpty(firstControllerContents))
+				applicantDto.setFirstControllerContents(firstControllerContents);
+			if (userId != null && userId > 0)
+				applicantDto.setUserId(userId);
+			if (applicantService.update(applicantDto) > 0) {
+				return new Response<Integer>(0, applicantDto.getId());
+			} else {
+				return new Response<Integer>(1, "修改失败.", 0);
+			}
+		} catch (ServiceException e) {
+			return new Response<Integer>(e.getCode(), e.getMessage(), 0);
+		}
+	}
+
+	@RequestMapping(value = "/count", method = RequestMethod.GET)
+	@ResponseBody
+	public Response<Integer> count(@RequestParam(value = "userId", required = false) Integer userId,
+			HttpServletRequest request, HttpServletResponse response) {
+		try {
+			return new Response<Integer>(0, applicantService.count(userId));
+		} catch (ServiceException e) {
+			return new Response<Integer>(1, e.getMessage(), null);
+		}
+	}
+
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	@ResponseBody
+	public ListResponse<List<ApplicantDTO>> list(@RequestParam(value = "userId", required = false) Integer userId,
+			@RequestParam(value = "pageNum") int pageNum, @RequestParam(value = "pageSize") int pageSize,
+			HttpServletRequest request, HttpServletResponse response) {
+		try {
+			int total = applicantService.count(userId);
+			List<ApplicantDTO> list = applicantService.list(userId, pageNum, pageSize);
+			return new ListResponse<List<ApplicantDTO>>(true, pageSize, total, list, "");
+		} catch (ServiceException e) {
+			return new ListResponse<List<ApplicantDTO>>(false, pageSize, 0, null, e.getMessage());
+		}
+	}
+
+	@RequestMapping(value = "/get", method = RequestMethod.GET)
+	@ResponseBody
+	public Response<ApplicantDTO> get(@RequestParam(value = "id") int id, HttpServletResponse response) {
+		try {
+			super.setGetHeader(response);
+			return new Response<ApplicantDTO>(0, applicantService.getById(id));
+		} catch (ServiceException e) {
+			return new Response<ApplicantDTO>(1, e.getMessage(), null);
+		}
+	}
+
+	@RequestMapping(value = "/delete", method = RequestMethod.GET)
+	@ResponseBody
+	public Response<Integer> delete(@RequestParam(value = "id") int id, HttpServletResponse response) {
+		try {
+			super.setGetHeader(response);
+			return new Response<Integer>(0, applicantService.deleteById(id));
+		} catch (ServiceException e) {
+			return new Response<Integer>(1, e.getMessage(), 0);
+		}
+	}
+
+}
