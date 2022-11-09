5378cd942b1b6ad5f859b9a3dc59a48fae2892c0
yjt <1126749228@qq.com>
2022-11-09 10:50:58 +0800

    rate高峰测试

--- src/main/java/org/zhinanzhen/b/service/impl/VisaOfficialServiceImpl.java
+++ src/main/java/org/zhinanzhen/b/service/impl/VisaOfficialServiceImpl.java
@@ -220,6 +220,7 @@ public class VisaOfficialServiceImpl extends BaseService implements VisaOfficial
                     this.add(7);
                     this.add(8);
                     this.add(9);
+                    this.add(11);
                 }
             };
             //长期签证计算
