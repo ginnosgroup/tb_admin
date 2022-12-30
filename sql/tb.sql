9f4488174ebaad85426b591ccfcc35ffbb9d5d03
Larry <larry7696@gmail.com>
2022-12-30 16:07:03 +0800

    work wx

--- sql/tb.sql
+++ sql/tb.sql
@@ -112,7 +112,7 @@ CREATE TABLE `tb_user` (
   `name` varchar(64) NOT NULL COMMENT '名称',
   `birthday` datetime NOT NULL COMMENT '生日',
   `phone` varchar(16) DEFAULT NULL COMMENT '电话号码',
-  `email` varchar(128) NOT NULL COMMENT '邮箱',
+  `email` varchar(128) DEFAULT NULL COMMENT '邮箱',
   `wechat_username` varchar(64) DEFAULT NULL COMMENT '微信帐号',
   `first_controller_contents` varchar(255) DEFAULT NULL COMMENT '初次咨询记录',
   `visa_code` varchar(16) DEFAULT NULL COMMENT '签证编号',
