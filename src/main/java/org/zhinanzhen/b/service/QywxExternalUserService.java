851de4ffca1aa29ac457fa183c0402bdc79e1a75
Larry <larry7696@gmail.com>
2023-01-05 13:03:36 +0800

    wxwork

--- src/main/java/org/zhinanzhen/b/service/QywxExternalUserService.java
+++ src/main/java/org/zhinanzhen/b/service/QywxExternalUserService.java
@@ -3,17 +3,24 @@ package org.zhinanzhen.b.service;
 import java.util.List;
 
 import org.zhinanzhen.b.service.pojo.QywxExternalUserDTO;
+import org.zhinanzhen.b.service.pojo.QywxExternalUserDescriptionDTO;
 import org.zhinanzhen.tb.service.ServiceException;
 
 public interface QywxExternalUserService {
 
 	int add(QywxExternalUserDTO qywxExternalUserDto) throws ServiceException;
 
+	int update(QywxExternalUserDTO qywxExternalUserDto) throws ServiceException;
+
 	int count(int adviserId, String state, String startDate, String endDate) throws ServiceException;
 
-	List<QywxExternalUserDTO> list(int adviserId, String state, String startDate, String endDate, int pageNum, int pageSize)
-			throws ServiceException;
+	List<QywxExternalUserDTO> list(int adviserId, String state, String startDate, String endDate, int pageNum,
+			int pageSize) throws ServiceException;
 
 	QywxExternalUserDTO getByExternalUserid(String externalUserid) throws ServiceException;
+
+	int updateDesc(QywxExternalUserDescriptionDTO qywxExternalUserDescriptionDto) throws ServiceException;
+
+	List<QywxExternalUserDescriptionDTO> listDesc(String externalUserid) throws ServiceException;
 
 }
