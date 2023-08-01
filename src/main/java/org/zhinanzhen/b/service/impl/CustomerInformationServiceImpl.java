49c288fb36b20966c0b4d4521ffea6172912cb2b
springriverer <1874260553@qq.com>
2023-08-01 12:01:48 +0800

    MMfile接口返回url

--- src/main/java/org/zhinanzhen/b/service/impl/CustomerInformationServiceImpl.java
+++ src/main/java/org/zhinanzhen/b/service/impl/CustomerInformationServiceImpl.java
@@ -459,8 +459,6 @@ public class CustomerInformationServiceImpl extends BaseService implements Custo
                 customerInformationDO.setMmdiskPath(mmdiskPath);
                 return customerInformationDO;
             }
-
-
             return null;
             /*if (ObjectUtil.isNotNull(customerInformationDO)&&ObjectUtil.isNotNull(customerInformationDO.getUrl())){
                 String givenName = customerInformationDO.getMainInformation().getGivenName();
