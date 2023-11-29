c1da9e02e237eefbacf37361465e6efd47e95460
Larry <larry7696@gmail.com>
2023-11-30 05:29:55 +0800

    文案权限

--- src/main/java/org/zhinanzhen/b/controller/nodes/ServiceOrderReviewNode.java
+++ src/main/java/org/zhinanzhen/b/controller/nodes/ServiceOrderReviewNode.java
@@ -98,15 +98,17 @@ public class ServiceOrderReviewNode extends SODecisionNode {
 				int serviceId = serviceOrderDto.getServiceId();
 				String officialIdStr = officialDto.getId() + "";
 				List<String> blackList = bOfficialReviewPermissions.get(serviceId);
-				if(ObjectUtil.isNotNull(blackList) && blackList.contains(officialIdStr)) {
-					context.putParameter("response", new Response<ServiceOrderDTO>(1,
-							StringUtil.merge("文案", officialDto.getName(), "不接该服务,请选择其他文案."), serviceOrderDto));
+				if (ObjectUtil.isNotNull(blackList) && blackList.contains(officialIdStr)) {
+					context.putParameter("response",
+							new Response<ServiceOrderDTO>(1,
+									StringUtil.merge("文案", officialDto.getName(), "不接该服务,请选择其他文案.如有疑问请联系管理员."),
+									serviceOrderDto));
 					return null;
 				}
 				List<String> whiteList = wOfficialReviewPermissions.get(serviceId);
-				if(ObjectUtil.isNull(whiteList) || !whiteList.contains(officialIdStr)) {
+				if (ObjectUtil.isNull(whiteList) || !whiteList.contains(officialIdStr)) {
 					context.putParameter("response", new Response<ServiceOrderDTO>(1,
-							StringUtil.merge("该服务属疑难,请选择其他资深文案."), serviceOrderDto));
+							StringUtil.merge("该服务属疑难,请选择其他资深文案.如有疑问请联系管理员."), serviceOrderDto));
 					return null;
 				}
 			}
