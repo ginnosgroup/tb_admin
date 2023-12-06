ef581b1f47ae44072f62da1e4bc76a381a0a0fe3
Larry <larry7696@gmail.com>
2023-12-07 05:21:46 +0800

    debug

--- src/main/java/org/zhinanzhen/tb/scheduled/Data.java
+++ src/main/java/org/zhinanzhen/tb/scheduled/Data.java
@@ -203,7 +203,7 @@ public class Data extends BaseService {
             });
 
             //计算dataDTOList每一行的total值
-			LOG.info(StringUtil.merge("dataDTOList_", startDate, "-", endDate, "_", opt, "_", dateMethod, " : ",
+			LOG.debug(StringUtil.merge("dataDTOList_", startDate, "-", endDate, "_", opt, "_", dateMethod, " : ",
 					dataDTOList));
             dataDTOList.forEach(dataDTO -> {
                 dataDTO.setTotal(dataDTO.getServiceFee()+dataDTO.getClaimCommission()+dataDTO.getDeductionCommission()+dataDTO.getAdjustments()-dataDTO.getRefunded());
