c6f5b39f862548f695737bb7f57672e996b60fe1
Awsister <1286559059@qq.com>
2024-09-20 17:50:54 +0800

    邮件bug修复

--- src/main/java/org/zhinanzhen/tb/utils/SendEmailUtil.java
+++ src/main/java/org/zhinanzhen/tb/utils/SendEmailUtil.java
@@ -2,7 +2,12 @@ package org.zhinanzhen.tb.utils;
 
 import com.ikasoa.web.utils.SimpleSendEmailTool;
 
+import com.sun.org.apache.bcel.internal.generic.NEW;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 @Slf4j
 public class SendEmailUtil {
@@ -11,41 +16,54 @@ public class SendEmailUtil {
 //			"EpibqJ2R6CFwvqiU", SimpleSendEmailTool.SmtpServerEnum.EXMAIL_QQ);
 	private static SimpleSendEmailTool txSendEmailTool = new SimpleSendEmailTool("leisu@zhinanzhen.org", "SuLei88",
 			SimpleSendEmailTool.SmtpServerEnum.EXMAIL_QQ);
-	private static SimpleSendEmailTool tx1SendEmailTool = new SimpleSendEmailTool("znznotice@zhinanzhen.org", "Znz630!",
+	private static SimpleSendEmailTool tx1SendEmailTool = new SimpleSendEmailTool("znznotice1@zhinanzhen.org", "Znzhen6300@",
 			SimpleSendEmailTool.SmtpServerEnum.EXMAIL_QQ);
-	private static SimpleSendEmailTool tx2SendEmailTool = new SimpleSendEmailTool("notice01@zhinanzhen.org", "Znz630!",
+	private static SimpleSendEmailTool tx2SendEmailTool = new SimpleSendEmailTool("znznotice2@zhinanzhen.org", "Znzhen6300@",
 			SimpleSendEmailTool.SmtpServerEnum.EXMAIL_QQ);
-	private static SimpleSendEmailTool tx3SendEmailTool = new SimpleSendEmailTool("tasknotice@zhinanzhen.org",
-			"Znz630!", SimpleSendEmailTool.SmtpServerEnum.EXMAIL_QQ);
+	private static SimpleSendEmailTool tx3SendEmailTool = new SimpleSendEmailTool("znznotice3@zhinanzhen.org", "Znzhen6300@",
+			SimpleSendEmailTool.SmtpServerEnum.EXMAIL_QQ);
+	private static SimpleSendEmailTool tx4SendEmailTool = new SimpleSendEmailTool("znznotice4@zhinanzhen.org", "Znzhen6300@",
+			SimpleSendEmailTool.SmtpServerEnum.EXMAIL_QQ);
+	private static SimpleSendEmailTool tx5SendEmailTool = new SimpleSendEmailTool("znznotice5@zhinanzhen.org", "Znzhen6300@",
+			SimpleSendEmailTool.SmtpServerEnum.EXMAIL_QQ);
+//	private static SimpleSendEmailTool tx3SendEmailTool = new SimpleSendEmailTool("tasknotice@zhinanzhen.org",
+//			"Znz630!", SimpleSendEmailTool.SmtpServerEnum.EXMAIL_QQ);
 
 //	private static SimpleSendEmailTool gmailSendEmailTool = new SimpleSendEmailTool("zhinanzhen630@gmail.com",
 //			"Zhinanzhen630", SimpleSendEmailTool.SmtpServerEnum.GMAIL);
 
+	private static List<SimpleSendEmailTool> simpleSendEmailTools = new ArrayList<SimpleSendEmailTool>();
+
+	public static void addSimpleSendEmailTools(SimpleSendEmailTool txSendEmailTool) {
+		simpleSendEmailTools.add(txSendEmailTool);
+	}
+
+	// 静态方法，用于获取静态List
+	public static List<SimpleSendEmailTool> getStaticList() {
+		if (simpleSendEmailTools != null && simpleSendEmailTools.size() == 0) {
+			SendEmailUtil.addSimpleSendEmailTools(tx1SendEmailTool);
+			SendEmailUtil.addSimpleSendEmailTools(tx2SendEmailTool);
+//			SendEmailUtil.addSimpleSendEmailTools(tx3SendEmailTool);
+//			SendEmailUtil.addSimpleSendEmailTools(tx4SendEmailTool);
+//			SendEmailUtil.addSimpleSendEmailTools(tx5SendEmailTool);
+		}
+		return simpleSendEmailTools;
+	}
+
 	public static void send(String mail, String title, String content) {
-//		mail = "leisu@zhinanzhen.org"; // 测试
 		log.info("发送邮件:" + mail + " | " + title + "|" + content);
 		Thread thread = new Thread(new Runnable() {
 			@Override
 			public void run() {
 				try {
-					tx1SendEmailTool.send(mail, title, content);
+					log.info(SendEmailUtil.getStaticList().toString());
+					List<SimpleSendEmailTool> simpleSendEmailToolsTmp = SendEmailUtil.getStaticList();
+					Random random = new Random();
+					int i = random.nextInt(2);
+					simpleSendEmailToolsTmp.get(i).send(mail, title, content);
 				} catch (Exception e) {
 					log.error(e.getMessage());
 					e.printStackTrace();
-					log.info("Try send mail again!");
-					try {
-						tx3SendEmailTool.send(mail, title, content);
-					} catch (Exception e1) {
-						log.error(e1.getMessage());
-						e1.printStackTrace();
-						log.info("Try send mail again! (2)");
-						try {
-							txSendEmailTool.send(mail, title, content);
-						} catch (Exception e2) {
-							log.error(e2.getMessage());
-							e2.printStackTrace();
-						}
-					}
 				}
 			}
 		});
