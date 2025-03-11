f87ded2b749d5c87eb4f0a2de79f9d06428ea49b
Awsister <1286559059@qq.com>
2025-03-11 15:04:10 +0800

    职业评估新增二级职业

--- src/main/java/org/zhinanzhen/b/dao/pojo/ServiceCategory.java
+++ src/main/java/org/zhinanzhen/b/dao/pojo/ServiceCategory.java
@@ -1,5 +1,13 @@
 package org.zhinanzhen.b.dao.pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.ibatis.type.Alias;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ServiceCategory {
 
     private  int id;
