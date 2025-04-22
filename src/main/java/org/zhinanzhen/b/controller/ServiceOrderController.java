84589a27337a90ce8cb062beec362e9be3a0dd8f
Awsister <1286559059@qq.com>
2025-04-22 17:23:29 +0800

    职评功能修改

--- src/main/java/org/zhinanzhen/b/controller/ServiceOrderController.java
+++ src/main/java/org/zhinanzhen/b/controller/ServiceOrderController.java
@@ -907,7 +907,7 @@ public class ServiceOrderController extends BaseController {
 							perAmount, amount, expectAmount, currency, exchangeRate, gst, deductGst, bonus, userId,
 							null, null, null, maraId, adviserId, officialId, remarks, closedReason, information,
 							isHistory, nutCloud, serviceAssessId, verifyCode, refNo, courseId,
-							schoolInstitutionLocationId, institutionTradingName, null, null, null, null, insuranceCompany, hasInsurance, isTransfer, transferRemarks, offerUrl, offerType);
+							schoolInstitutionLocationId, institutionTradingName, null, null, null, null, insuranceCompany, hasInsurance, isTransfer, transferRemarks, servicePackageIds, offerUrl, offerType);
 					if (cRes.getCode() > 0)
 						res.setMessage(res.getMessage() + ";" + cRes.getMessage());
 				});
