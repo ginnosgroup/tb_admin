4f33c5fb44274f2dbdfd571b2791c43d1b6c3bc2
Awsister <1286559059@qq.com>
2026-05-29 17:37:44 +0800

    发送邮件调用线程池

--- src/main/java/org/zhinanzhen/tb/utils/SendEmailUtil.java
+++ src/main/java/org/zhinanzhen/tb/utils/SendEmailUtil.java
@@ -2,7 +2,7 @@ package org.zhinanzhen.tb.utils;
 
 import com.ikasoa.web.utils.SimpleSendEmailTool;
 
-import com.sun.org.apache.bcel.internal.generic.NEW;
+import org.zhinanzhen.b.config.GlobalThreadPool;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.File;
@@ -84,50 +84,40 @@ public class SendEmailUtil {
 	}
 
 	public static void send(String mail, String title, String content) {
-		log.info("发送邮件:" + mail + " | " + title + "|" + content);
-		Thread thread = new Thread(new Runnable() {
-			@Override
-			public void run() {
-				try {
-					log.info(SendEmailUtil.getStaticList().toString());
-					List<SimpleSendEmailTool> simpleSendEmailToolsTmp = SendEmailUtil.getStaticList();
-					Random random = new Random();
-					int i = random.nextInt(2);
-					simpleSendEmailToolsTmp.get(i).send(mail, title, content);
-				} catch (Exception e) {
-					log.error(e.getMessage());
-					e.printStackTrace();
-				}
+		log.info("发送邮件: " + mail + " | " + title + "|" + content);
+		GlobalThreadPool.getInstance().execute(() -> {
+			try {
+				log.info(SendEmailUtil.getStaticList().toString());
+				List<SimpleSendEmailTool> simpleSendEmailToolsTmp = SendEmailUtil.getStaticList();
+				Random random = new Random();
+				int i = random.nextInt(2);
+				simpleSendEmailToolsTmp.get(i).send(mail, title, content);
+			} catch (Exception e) {
+				log.error(e.getMessage());
+				e.printStackTrace();
 			}
 		});
-		thread.start();
-		log.info("发送邮件完成:" + mail);
+		log.info("发送邮件完成: " + mail);
 	}
 
 	public static void sendExcel(String mail, String title, String content, File file) {
-		log.info("发送邮件:" + mail + " | " + title + "|" + content);
-		Thread thread = new Thread(new Runnable() {
-			@Override
-			public void run() {
-				try {
-					log.info(SendEmailUtil.getStaticListTmp().toString());
-					List<EmailService> simpleSendEmailToolsTmp = SendEmailUtil.getStaticListTmp();
-					// 检查列表是否为空
-					if (simpleSendEmailToolsTmp.isEmpty()) {
-						log.error("邮件服务列表为空，无法发送邮件！");
-						return;
-					}
-					Random random = new Random();
-					int i = random.nextInt(2);
-					simpleSendEmailToolsTmp.get(i).send(mail, title, content, file);
-				} catch (Exception e) {
-					log.error(e.getMessage());
-					e.printStackTrace();
+		log.info("发送邮件: " + mail + " | " + title + "|" + content);
+		GlobalThreadPool.getInstance().execute(() -> {
+			try {
+				log.info(SendEmailUtil.getStaticListTmp().toString());
+				List<EmailService> simpleSendEmailToolsTmp = SendEmailUtil.getStaticListTmp();
+				if (simpleSendEmailToolsTmp.isEmpty()) {
+					log.error("邮件服务列表为空，无法发送邮件！");
+					return;
 				}
+				Random random = new Random();
+				int i = random.nextInt(2);
+				simpleSendEmailToolsTmp.get(i).send(mail, title, content, file);
+			} catch (Exception e) {
+				log.error(e.getMessage());
+				e.printStackTrace();
 			}
 		});
-		thread.start();
-		log.info("发送邮件完成:" + mail);
+		log.info("发送邮件完成: " + mail);
 	}
-
 }
